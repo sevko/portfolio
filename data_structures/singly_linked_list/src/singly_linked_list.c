c61e6c09ccb08093e2e6a64b3aff087328ff7d17
Severyn Kozak <severyn.kozak@gmail.com>
2014-06-28 00:02:26 -0400

    Define createSLList(), freeSLList(), freeSLNode().
    data_structures/singly_linked_list/src/singly_linked_list.(c, h)
    	-Add definitions and documentation for `createSLList()`,
    	`freeSLList()`, and `freeSLNode()`.

--- data_structures/singly_linked_list/src/singly_linked_list.c
+++ data_structures/singly_linked_list/src/singly_linked_list.c
@@ -3,10 +3,29 @@
 
 #include "singly_linked_list.h"
 
-SLList_t *createSLList(void){
+/*
+ * @brief Deallocate an ::SLNode_t
+ *
+ * @param node A node.
+ *
+ * @return The deallocated node's ::data.
+*/
+static void *freeSLNode(SLNode_t *node);
+
+SLList_t *createSLList(void (*freeData)(void *data)){
+	SLList_t *list = malloc(sizeof(SLList_t));
+	list->head = list->tail = NULL;
+	list->len = 0;
+	return list;
 }
 
 void freeSLList(SLList_t *list){
+	SLNode_t *currNode = list->head;
+	while(currNode != NULL){
+		SLNode_t *nextNode = currNode->next;
+		list->freeData(freeSLNode(currNode));
+		currNode = nextNode;
+	}
 }
 
 void insertAfterSLNode(SLList_t *list, SLNode_t *node){
@@ -15,6 +34,11 @@ void insertAfterSLNode(SLList_t *list, SLNode_t *node){
 void removeAfterSLNode(SLList_t *list, SLNode_t *node){
 }
 
+static void *freeSLNode(SLNode_t *node){
+	void *data = node->data;
+	free(node);
+	return data;
+}
 
 int main(){
 	puts("Hello world.");
--- data_structures/singly_linked_list/src/singly_linked_list.h
+++ data_structures/singly_linked_list/src/singly_linked_list.h
@@ -19,9 +19,24 @@ typedef struct {
 	int len; // The number of ::SLNode_t inside the list.
 	SLNode_t *head, // The head of the list.
 		*tail; // The tail of the list.
+	void (*freeData)(void *data);
 } SLList_t;
 
-SLList_t *createSLList(void);
+/*
+ * @brief Allocate an ::SLList_t.
+ *
+ * @param freeData A function to free the data to be stored inside ::SLNode_t
+ *      inside the returned ::SLList_t.
+ *
+ * @return A pointer to the new ::SLList_t.
+*/
+SLList_t *createSLList(void (*freeData)(void *data));
+
+/*
+ * @brief Deallocate an ::SLList_t.
+ *
+ * @param list The list to be free'd.
+*/
 void freeSLList(SLList_t *list);
 void insertAfterSLNode(SLList_t *list, SLNode_t *node);
 void removeAfterSLNode(SLList_t *list, SLNode_t *node);
