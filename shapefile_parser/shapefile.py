91ee8e064cb724b7827a369361a6b4be482008b4
Severyn Kozak <severyn.kozak@gmail.com>
2014-07-01 10:53:45 -0400

    Add Shape, BoundingBox, _handleCommandLineArgs().
    shapefiles/shapefile.py
    	-Add `Shape`, `BoundingBox` classes.
    	-Migrate argument handling code from `main` to a new function,
    	`_handleCommandLineArgs()`, for modularity.
    	-Replace `Reader` and `Writer` classes with `read_from_file()`
    	and `write_to_file()` respectively.

--- shapefile_parser/shapefile.py
+++ shapefile_parser/shapefile.py
@@ -4,26 +4,68 @@ A module for ESRI Shapefile reading, and writing.
 
 import sys
 
+class Shape(object):
+	"""
+	Contains shapefile shape types.
+	"""
+
+	NULL_SHAPE = 0
+	POINT = 1
+	POLY_LINE = 3
+	POLYGON = 5
+	MULTI_POINT = 8
+	POINT_Z = 11
+	POLY_LINE_Z = 13
+	POLYGON_Z = 15
+	MULTI_POINT_Z = 18
+	POINT_M = 21
+	POLY_LINE_M = 23
+	POLYGON_M = 25
+	MULTI_POINT_M = 28
+	MULTI_PATCH = 31
+
 class Shapefile(object):
+	"""
 
-	def __init__(self):
+	Attributes:
+		length (int): The number of bytes in the file.
+		shape_type (int): Any of the constant instances in `Shape`.
+		bounding_box (BoundingBox): The shapefile's bounds.
+	"""
+
+	def __init__(self, length=None, shape_type=None, bounding_box=None):
+		self.length = length
+		self.shape_type = shape_type
+		self.bounding_box = bounding_box
+
+	def read_from_file(path):
 		pass
 
-class Reader(object):
-
-	def __init__(self):
+	def write_to_file(path):
 		pass
 
-class Writer(object):
+class BoundingBox(object):
 
-	def __init__(self):
-		pass
+	def __init__(self, min_x, max_x, min_y, max_y, min_m=None, max_m=None,
+			min_z=None, max_z=None):
+		self.min_x = min_x
+		self.max_x = max_x
+		self.min_y = min_y
+		self.max_y = max_y
+		self.min_m = min_m
+		self.max_m = max_m
+		self.min_z = min_z
+		self.max_z = max_z
 
-if __name__ == "__main__":
+def _handleCommandLineArgs():
 	if 1 < len(sys.argv):
-		print "Reading %s." % sys.argv[1]
+		shapefile = Shapefile()
+		shapefile.read_from_file(sys.argv[1])
 	else:
 		print (
 			"Missing argument. Use:\n"
 			"\tpython shapefile_parser.py SHAPEFILE_PATH\n\n"
 			"\tSHAPEFILE_PATH : the path of a shapefile (.shp) file.")
+
+if __name__ == "__main__":
+	_handleCommandLineArgs()
