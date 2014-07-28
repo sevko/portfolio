c57fdbbf078609dd51e91cfdd183f28f5031704e
Severyn Kozak <severyn.kozak@gmail.com>
2014-07-28 15:16:56 -0400

    Add all func headers, implement several.
    data_structures/circular_singly_linked_list/makefile
    	-Compile the executable into `bin/` instead of the project root.
    
    data_structures/circular_singly_linked_list/src/
    	circular_singly_linked_list.c
    	-Add all necessary circular-singly-linked-list function headers.
    	-Add `CSLNode`, `CSLList_t` struct declarations.
    	-Define `createCSLList()`, `freeCSLList()`, and `freeCSLNode()`.
    
    data_structures/circular_singly_linked_list/src/test.c
    	-Add a `test` module to test all functions implemented in
    	`circular_singly_linked_list.(c, h)`; a separate file is
    	necessary to maintain the modularity of the main list
    	implementation, which should theoretically be easily
    	`include`-able in any project.

--- data_structures/circular_singly_linked_list/src/circular_singly_linked_list.c
+++ data_structures/circular_singly_linked_list/src/circular_singly_linked_list.c
@@ -3,7 +3,52 @@
 
 #include "circular_singly_linked_list.h"
 
-int main(){
-	puts("Hello world.");
-	return EXIT_SUCCESS;
+// A singly-linked node that composes an ::CSLList_t.
+typedef struct CSLNode {
+	void *data; // The contained data.
+	struct CSLNode *next; // The next node in the sequence.
+} CSLNode_t;
+
+// A singly-linked-list.
+typedef struct {
+	int len; // The number of ::CSLNode_t inside the list.
+	CSLNode_t *head; // The beginning of the list.
+	// Function to deallocate the data stored inside this list's ::CSLNode_t.
+	void (*freeData)(void *data);
+} CSLList_t;
+
+void insertCSLListHead(CSLList_t *list, void *data);
+void *removeCSLListHead(CSLList_t *list);
+void insertAfterCSLNode(CSLList_t *list, CSLNode_t *node, void *data);
+void *removeAfterCSLNode(CSLList_t *list, CSLNode_t *node);
+void insertAtIndex(CSLList_t *list, int index, void *data);
+void *removeAtIndex(CSLList_t *list, int index);
+void *removeCSLNode(CSLList_t *list, CSLNode_t *node);
+static CSLNode_t *createCSLNode(void *data);
+static void *freeCSLNode(CSLNode_t *node);
+
+CSLList_t *createCSLList(void (*freeData)(void *data)){
+	CSLList_t *list = malloc(sizeof(CSLList_t));
+	list->head = NULL;
+	list->len = 0;
+	list->freeData = freeData;
+	return list;
+}
+
+void freeCSLList(CSLList_t *list){
+	CSLNode_t *currNode = list->head;
+	int ind = 0;
+
+	while(ind < list->len){
+		CSLNode_t *nextNode = currNode->next;
+		list->freeData(freeCSLNode(currNode));
+		currNode = nextNode;
+		ind++;
+	}
+}
+
+static void *freeCSLNode(CSLNode_t *node){
+	void *data = node->data;
+	free(node);
+	return data;
 }
---
+++ data_structures/circular_singly_linked_list/src/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "src/circular_singly_linked_list.h"
+
+static void testCSLList(void);
+
+static void testCSLList(void){
+	puts("Hello world.");
+}
+
+int main(){
+	testCSLList();
+	return EXIT_SUCCESS;
+}
