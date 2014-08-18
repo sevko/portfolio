26d0bc9bcc36252e165d015caae22936dbedf0a4
Severyn Kozak <severyn.kozak@gmail.com>
2014-08-18 10:07:58 -0400

    Fin. the polynomial root algorithm implementation.
    algorithms/polynomial_roots/polynomial_roots.py
    	-Complete my implementation of the polynomial-root estimation
    	algorithm.
    	-Add a docstring to `polynomial_root()` and the module header.

--- algorithms/polynomial_roots/polynomial_roots.py
+++ algorithms/polynomial_roots/polynomial_roots.py
@@ -1,4 +1,29 @@
+"""
+An implementation of Newton's polynomial root estimation algorithm.
+"""
+
 def polynomial_root(polynomial, derivative, x, max_delta=1e-6):
+	"""
+	Estimate the root of a polynomial using Newton's method.
+
+	Args:
+		polynomial (func): A function that represents the polynomial whose root
+			will be found. Must take one numeric argument, and return a numeric
+			value.
+		derivative (func): A function that represents the derivative of
+			`polynomial`. Must take one numeric argument, and return a numeric
+			value.
+		x (float): A good guess (per the standards of Newton's method) for a
+			root of the polynomial, used as a seed for the algorithm.
+		max_delta (float): The maximum difference between subsequent
+			estimations of a root that can be attained before the estimate is
+			returned as the root. Smaller values result in greater precision,
+			but require a few more iterations of the algorithm.
+
+	Returns:
+		(float) An estimated root of `polynomial`, found relatively near `x`.
+	"""
+
 	prev_x = x - 2 * max_delta # So as to fail the while-loop conditional.
 	while max_delta < abs(x - prev_x):
 		prev_x = x
