377f86527ab4d23cdb23cbf1a1480ba1e0626015
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-17 22:49:08 -0400

    Add skeleton Set implementation, README.
    data_structures/set/README.md
    	-Add a short README, to contain much more content in the future.
    
    data_structures/set/index.js
    	-Add a skeleton implementation of a Set, structured as a Node
    	package.
    	-Define `Set()` with some member variables and methods declared,
    	and a placeholder `test()`.

---
+++ data_structures/set/README.md
@@ -0,0 +1,2 @@
+# set
+An implementation of a [set](http://en.wikipedia.org/wiki/Set_(abstract_data_type)) data-structure.
---
+++ data_structures/set/index.js
@@ -0,0 +1,35 @@
+function test(){
+	console.log("Testing.");
+}
+
+function Set(){
+	this.length = 0;
+	this.items = [];
+
+	this.insert = function insert(item){
+	};
+
+	this.remove = function remove(item){
+	};
+
+	this.union = function union(set){
+	};
+
+	this.intersection = function intersection(set){
+	};
+
+	this.difference = function difference(set){
+	};
+
+	this.isMember = function isMember(item){
+	};
+
+	this.isSubset = function isSubset(set){
+	};
+
+	this.isEqual = function isEqual(set){
+	};
+}
+
+module.exports = Set;
+test();
