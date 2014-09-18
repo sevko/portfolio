86a4c5841ef4cb95cea64c13ec314427fa2164ea
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-18 09:24:04 -0400

    Add unit-tests for isMember(), length().
    data_structures/set/test/test.js
    	-Define unit-tests for `Set.isMember()` and `Set.length()`.

--- data_structures/set/test/test.js
+++ data_structures/set/test/test.js
@@ -7,6 +7,28 @@
 var assert = require("assert");
 var Set = require("../index.js");
 
+describe("isMember()", function(){
+	it("Should find existing items.", function(){
+		var set = new Set();
+		set.insert(1);
+		assert(set.isMember(1));
+	});
+	it("Should not find inexistent items.", function(){
+		var set = new Set();
+		set.insert(2);
+		assert(!set.isMember(1));
+	});
+});
+
+describe("length()", function(){
+	it("Should report number of items in the set.", function(){
+		var set = new Set();
+		assert(set.length() == 0);
+		set.insert(1);
+		assert(set.length() == 1);
+	});
+});
+
 describe("insert()", function(){
 	it("Should insert items.", function(){
 		var set = new Set();
