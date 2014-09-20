9623995cccd47d22b1eede29e31050ea3b11fa4e
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-19 23:04:06 -0400

    Integrate Set(), and implement findSetCover().
    algorithms/set_cover/index.js
    	-Require the `Set` data-structure, implemented in
    	`/data_structures/set/`.
    	-Implement and document `findSetCover()`.

--- algorithms/set_cover/index.js
+++ algorithms/set_cover/index.js
@@ -4,11 +4,14 @@
 
 "use strict";
 
+var Set = require("../../data_structures/set/"); // jshint ignore:line
+
 /**
  * Generate all subsets of a array.
  *
  * @param {array} array An array.
- * @return {array of arrays} The set of all subsets of `array`.
+ * @return {array of arrays} The set of all subsets of `array`, ordered by
+ *      length.
  */
 function generateSubsets(array){
 	/**
@@ -50,4 +53,30 @@ function generateSubsets(array){
 		}
 	}
 	return subsets;
+}
+
+/**
+ * Find the set cover for an array of sets.
+ *
+ * @param {array of sets} sets An array of sets.
+ * @return {array of sets} The smallest subset of `sets`, whose union is equal
+ *      to the union of all sets inside `sets`; in other words, the set cover
+ *      for `sets`.
+ */
+function findSetCover(sets){
+	var universe = new Set();
+	for(var set = 0; set < sets.length; set++){
+		universe = universe.union(sets[set]);
+	}
+
+	var subsets = generateSubsets(sets);
+	for(var subset = 0; subset < subsets.length; subset++){
+		var union = new Set();
+		for(set = 0; set < subsets[subset].length; set++){
+			union = union.union(subsets[subset][set]);
+		}
+		if(union.isEqual(universe)){
+			return subsets[subset];
+		}
+	}
 }
