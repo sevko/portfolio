966fc2cda9ecd4a41a37e74e978f900f6f72a9c0
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-19 22:33:31 -0400

    Add documentation to index.js; rename subsets().
    algorithms/set_cover/index.js
    	-Rename `subsets()` to `generateSubsets()`.
    	-Add documentation comment to file-header, `generateSubsets()`,
    	and `fixedLengthSubsets()`.

--- algorithms/set_cover/index.js
+++ algorithms/set_cover/index.js
@@ -1,6 +1,27 @@
+/**
+ * @file An implementation of a solution to the set-covering problem.
+ */
+
 "use strict";
 
-function subsets(array){
+/**
+ * Generate all subsets of a array.
+ *
+ * @param {array} array An array.
+ * @return {array of arrays} The set of all subsets of `array`.
+ */
+function generateSubsets(array){
+	/**
+	 * Recursively generate all subsets of a certain length.
+	 *
+	 * @param {int} subsetLen The length of each of the generated subsets.
+	 * @param {int} itemNum The number of the item located at `startInd` of the
+	 *      superset array, relative to the numbers already added to the subset
+	 *      by preceding calls to `fixedLengthSubsets()`. Used to moderate
+	 *      recursion, which terminates when `itemNum` equals `subsetLen`.
+	 * @param {int} startInd The index to start generating recursive subsets
+	 *      from.
+	 */
 	function fixedLengthSubsets(subsetLen, itemNum, startInd){
 		if(itemNum === subsetLen){
 			return [[]];
@@ -30,5 +51,3 @@ function subsets(array){
 	}
 	return subsets;
 }
-
-console.log(subsets([1, 2, 3, 4]));
