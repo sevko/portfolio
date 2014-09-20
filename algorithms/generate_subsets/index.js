cc7eac1550eb8fd1fe48cad99650d74e9fe53152
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-20 13:22:08 -0400

    Move set_cover.generateSubsets() to its own module.
    algorithms/(set_cover, generate_subsets)/index.js
    	-Move `generateSubsets()` from `set_cover/index.js` to its own
    	module, `generate_subsets/index.js`.
    	-`require()` in the module for use in `findSetCover()`.

---
+++ algorithms/generate_subsets/index.js
@@ -0,0 +1,65 @@
+/**
+ * @file An implementation of an algorithm that finds a set's subsets.
+ */
+
+var util = require("util");
+
+/**
+ * Generate all subsets of an array.
+ *
+ * @param {array} array An array.
+ * @return {array} The set of all subsets of `array`, ordered by
+ *      length.
+ */
+function generateSubsets(array){
+	/**
+	 * Recursively generate all subsets of a certain length.
+	 *
+	 * @param {int} subsetLen The length of each of the generated subsets.
+	 * @param {int} itemNum The number of the item located at `startInd` of the
+	 *      superset array, relative to the numbers already added to the subset
+	 *      by preceding calls to `fixedLengthSubsets()`. Used to moderate
+	 *      recursion, which terminates when `itemNum` equals `subsetLen`.
+	 * @param {int} startInd The index to start generating recursive subsets
+	 *      from.
+	 */
+	function fixedLengthSubsets(subsetLen, itemNum, startInd){
+		if(itemNum === subsetLen){
+			return [[]];
+		}
+		else {
+			var subsets = [];
+			var endInd = array.length - (subsetLen - itemNum - 1);
+			for(var ind = startInd; ind < endInd; ind++){
+				var fixedSubsets = fixedLengthSubsets(
+					subsetLen, itemNum + 1, ind + 1
+				);
+				for(var subset = 0; subset < fixedSubsets.length; subset++){
+					fixedSubsets[subset].push(array[ind]);
+					subsets.push(fixedSubsets[subset]);
+				}
+			}
+			return subsets;
+		}
+	}
+
+	var subsets = [];
+	for(var subsetLen = 1; subsetLen <= array.length; subsetLen++){
+		var fixedLenSubsets = fixedLengthSubsets(subsetLen, 0, 0);
+		for(var subset = 0; subset < fixedLenSubsets.length; subset++){
+			subsets.push(fixedLenSubsets[subset]);
+		}
+	}
+	return subsets;
+}
+
+(function testSetCover(){
+	var set = [1, 2, 3, 4];
+	var subsets = generateSubsets(set);
+	console.log(util.format(
+		"Original set: %s\nNumber of subsets: %d\nSubsets:\n%s",
+		set, subsets.length, subsets.join("\n")
+	));
+})();
+
+module.exports = generateSubsets;
--- algorithms/set_cover/index.js
+++ algorithms/set_cover/index.js
@@ -5,55 +5,7 @@
 "use strict";
 
 var Set = require("../../data_structures/set/"); // jshint ignore:line
-
-/**
- * Generate all subsets of a array.
- *
- * @param {array} array An array.
- * @return {array of arrays} The set of all subsets of `array`, ordered by
- *      length.
- */
-function generateSubsets(array){
-	/**
-	 * Recursively generate all subsets of a certain length.
-	 *
-	 * @param {int} subsetLen The length of each of the generated subsets.
-	 * @param {int} itemNum The number of the item located at `startInd` of the
-	 *      superset array, relative to the numbers already added to the subset
-	 *      by preceding calls to `fixedLengthSubsets()`. Used to moderate
-	 *      recursion, which terminates when `itemNum` equals `subsetLen`.
-	 * @param {int} startInd The index to start generating recursive subsets
-	 *      from.
-	 */
-	function fixedLengthSubsets(subsetLen, itemNum, startInd){
-		if(itemNum === subsetLen){
-			return [[]];
-		}
-		else {
-			var subsets = [];
-			var endInd = array.length - (subsetLen - itemNum - 1);
-			for(var ind = startInd; ind < endInd; ind++){
-				var fixedSubsets = fixedLengthSubsets(
-					subsetLen, itemNum + 1, ind + 1
-				);
-				for(var subset = 0; subset < fixedSubsets.length; subset++){
-					fixedSubsets[subset].push(array[ind]);
-					subsets.push(fixedSubsets[subset]);
-				}
-			}
-			return subsets;
-		}
-	}
-
-	var subsets = [];
-	for(var subsetLen = 1; subsetLen <= array.length; subsetLen++){
-		var fixedLenSubsets = fixedLengthSubsets(subsetLen, 0, 0);
-		for(var subset = 0; subset < fixedLenSubsets.length; subset++){
-			subsets.push(fixedLenSubsets[subset]);
-		}
-	}
-	return subsets;
-}
+var generateSubsets = require("../generate_subsets/");
 
 /**
  * Find the set cover for an array of sets.
