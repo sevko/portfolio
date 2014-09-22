f1adaca081547d9799375836d056ce1050b4f046
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-21 21:24:09 -0400

    Add a completed implementation of power_set_3.
    algorithms/power_set_3/index.js
    	-Define/document a completed `powerSet()`.

--- algorithms/power_set_3/index.js
+++ algorithms/power_set_3/index.js
@@ -4,4 +4,28 @@
 
 "use strict";
 
-console.log("Hello, world.");
+/**
+ * Generate all subsets of an array.
+ *
+ * Uses the binary form of an integer to indicate which values are present in
+ * any subset of the given set.
+ *
+ * @param {array} array An array.
+ * @return {array} The set of all subsets of `array`.
+ */
+function powerSet(array){
+	var subsets = [];
+	for(var subset = 0; subset < Math.pow(2, array.length); subset++){
+		var newSubset = [];
+		for(var bit = 0; bit < array.length; bit++){
+			if((subset >> bit) & 1){
+				newSubset.push(array[bit]);
+			}
+		}
+		subsets.push(newSubset);
+	}
+
+	return subsets;
+}
+
+module.exports = powerSet;
