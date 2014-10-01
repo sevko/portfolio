3db07fdbe885508714c78da0e4a16e21fbb1c859
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-30 21:54:46 -0400

    Define ulamSpiral() in spirals.js.
    visualizations/prime_spirals/spirals.js
    	-Define a completed and functional `ulamSpiral()`.
    	-Add documentation comment.

--- visualizations/prime_spirals/spirals.js
+++ visualizations/prime_spirals/spirals.js
@@ -1,1 +1,32 @@
-var drawPixel = setupCanvas(300);
+/**
+ * Render an Ulam spiral on the page's canvas.
+ *
+ * @param {number} numLayers The number of concentric rings, or layers, in the
+ *      spiral.
+ */
+function ulamSpiral(numLayers){
+	var drawPixel = setupCanvas(numLayers);
+
+	var currValue = 1;
+	var x = 0;
+	var y = 0;
+
+	for(var layer = 0, len = 0; layer <= numLayers; layer++, len += 2){
+		function drawLine(dx, dy, len){
+			for(var pixel = 0; pixel < len; pixel++){
+				if(primality(currValue++)){
+					drawPixel(x, y);
+				}
+				x += dx;
+				y += dy;
+			}
+		}
+
+		drawLine(0, -1, len - 1);
+		drawLine(-1, 0, len);
+		drawLine(0, 1, len);
+		drawLine(1, 0, len + 1);
+	}
+}
+
+ulamSpiral(300);
