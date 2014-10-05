19f9a8912196e75810ea856227897b5bfa8db5ee
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-05 13:43:47 -0400

    Add skeleton open_addressed_hash_table module.
    data_structures/open_addressed_hash_table/open_addressed_hash_table.py
    	-Declare `OpenAddressedHashTable` class, with all of the
    	necessary skeleton methods.

---
+++ data_structures/open_addressed_hash_table/open_addressed_hash_table.py
@@ -0,0 +1,21 @@
+class OpenAddressedHashTable(object):
+	def __init__(self, num_buckets, hash_func, items=None):
+		pass
+
+	def insert(self, item):
+		pass
+
+	def remove(self, item):
+		pass
+
+	def contains(self, item):
+		pass
+
+	def load_factor(self):
+		pass
+
+	def __len__(self):
+		pass
+
+	def __str__(self):
+		pass
