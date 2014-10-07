2c15e431e732c431d8173bba13489e6fdf978996
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-06 23:04:16 -0400

    Define tap unit-tests for several funcs.
    data_structures/binary_tree/src/test.c
    	-Use `libtap` to write TAP unit-tests for the project.
    	-Define `test_create()`, `test_insertRoot()`, and
    	`test_insertLeft()`.

--- data_structures/binary_tree/src/test.c
+++ data_structures/binary_tree/src/test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <tap.h>
 
 #include "src/binary_tree.h"
 
@@ -7,11 +9,45 @@ void freeData(void *data){
 	free((char *)data);
 }
 
-void testBinaryTree(void){
+static void test_create(void){
 	BinaryTree_Tree_t *tree = BinaryTree_create(freeData);
+	plan(NO_PLAN);
+	ok(tree->size == 0, "Tree size set to 0.");
+	ok(tree->root == NULL, "Tree root is null.");
+}
+
+static void test_insertRoot(void){
+	char *data = malloc(2);
+	strcpy(data, "a");
+	BinaryTree_Tree_t *tree = BinaryTree_create(freeData);
+	BinaryTree_insertRoot(tree, data);
+	ok(tree->size == 1, "Tree size is 1.");
+	is(tree->root->data, data, "Root node contains correct data.");
+}
+
+static void test_insertLeft(void){
+	const int numData = 3;
+	char *data[numData];
+	for(int item = 0; item < numData; item++){
+		data[item] = malloc(2);
+		data[item][0] = item + (int)'a';
+	}
+
+	BinaryTree_Tree_t *tree = BinaryTree_create(freeData);
+	BinaryTree_insertRoot(tree, data[0]);
+
+	BinaryTree_insertLeft(tree, tree->root, data[1]);
+	is(tree->root->left->data, data[1], "Leaf node inserted correctly.");
+
+	BinaryTree_insertLeft(tree, tree->root, data[2]);
+	is(tree->root->left->data, data[2], "Non-leaf node inserted correctly.");
+	is(tree->root->left->left->data, data[1], "Existing nodes preserved.");
 }
 
 int main(){
-	testBinaryTree();
+	test_create();
+	test_insertRoot();
+	test_insertLeft();
+	done_testing();
 	return EXIT_SUCCESS;
 }
