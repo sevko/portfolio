f74c4f9d6c1cfe2e0df0d85fdec32ff108e0ab0f
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-06 22:05:46 -0400

    Define BinaryTree_ insertLeft() and insertRight().
    data_structures/binary_tree/src/binary_tree.(c, h)
    	-Define and document `BinaryTree_insertLeft()` and
    	`BinaryTree_insertRight()`; not tested.
    
    data_structures/binary_tree/src/binary_tree.c
    	-Define and document `static` method `createNode()`.

--- data_structures/binary_tree/src/binary_tree.c
+++ data_structures/binary_tree/src/binary_tree.c
@@ -3,10 +3,43 @@
 
 #include "binary_tree.h"
 
+/*
+ * @brief Allocate memory for a new node.
+ * @param data The `data` member of the new node.
+ * @return A pointer to the new node.
+*/
+static BinaryTree_Node_t *createNode(void *data);
+
 BinaryTree_Tree_t *BinaryTree_create(void (*freeData)(void *data)){
 	BinaryTree_Tree_t *tree = malloc(sizeof(BinaryTree_Tree_t));
 	tree->size = 0;
 	tree->freeData = freeData;
 	tree->root = NULL;
 	return tree;
+}
+
+void BinaryTree_insertLeft(
+	BinaryTree_Tree_t *tree, BinaryTree_Node_t *node, void *data
+){
+	BinaryTree_Node_t *newNode = BinaryTree_createNode(data);
+	newNode->left = node->left;
+	node->left = newNode;
+	tree->size++;
+}
+
+void BinaryTree_insertRight(
+	BinaryTree_Tree_t *tree, BinaryTree_Node_t *node, void *data
+){
+	BinaryTree_Node_t *newNode = BinaryTree_createNode(data);
+	newNode->left = node->left;
+	node->left = newNode;
+	tree->size++;
+}
+
+static BinaryTree_Node_t *createNode(void *data){
+	BinaryTree_Node_t *node = malloc(sizeof(BinaryTree_Node_t));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	return node;
 }
--- data_structures/binary_tree/src/binary_tree.h
+++ data_structures/binary_tree/src/binary_tree.h
@@ -17,14 +17,27 @@ typedef struct {
  * @return A pointer to the new, empty tree.
 */
 BinaryTree_Tree_t *BinaryTree_create(void (*freeData)(void *data));
+
 void BinaryTree_free(BinaryTree_Tree_t *tree);
+
+/**
+ * @brief Create and insert a node as the left child of another node.
+ *
+ * @param tree The tree containing 'node'.
+ * @param node The node into whose `left` position a new node will be
+ *      inserted; it'll inherit the current value of `left`.
+ * @param data The `data` member of the newly created node.
+*/
 void BinaryTree_insertLeft(
-	BinaryTree_Tree_t *tree, BinaryTree_Node_t *node,
-	void *data
+	BinaryTree_Tree_t *tree, BinaryTree_Node_t *node, void *data
 );
+
+/**
+ * See `BinaryTree_insertLeft`: works the same way, but operates on nodes'
+ * `right` member instead of their `left`.
+*/
 void BinaryTree_insertRight(
-	BinaryTree_Tree_t *tree, BinaryTree_Node_t *node,
-	void *data
+	BinaryTree_Tree_t *tree, BinaryTree_Node_t *node, void *data
 );
 void *BinaryTree_removeLeft(
 	BinaryTree_Tree_t *tree, BinaryTree_Tree_t *node
