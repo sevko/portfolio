fe6fdf0aa8f5afe79ea7245a9c92c8a8be6cc759
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-09 22:51:48 -0400

    Compile in binary_tree, add func prototypes.
    data_structures/avl_tree/makefile
    	-Compile in the `binary_tree` project, which exposes
    	`BinaryTree_Tree_t` data structure that `AVL_Tree_t` will rely
    	on.
    	-Add necessary compiler flags, etc.
    
    data_structures/avl_tree/src/avl_tree.h
    	-Add prototypes for all necessary functions.
    	-Declare `AVL_Node_t`.

--- data_structures/avl_tree/src/avl_tree.h
+++ data_structures/avl_tree/src/avl_tree.h
@@ -1,5 +1,22 @@
 /**
- * @brief 
+ * @brief An implementation of an AVL tree.
  */
 
 #pragma once
+
+#include "binary_tree.h"
+
+// One node of the AVL tree.
+typedef struct {
+	void *data; // The data contained inside this node.
+	int balanceFactor; // The balance factor of this node.
+} AVL_Node_t;
+
+// An AVL tree.
+typedef BinaryTree_Tree_t AVL_Tree_t;
+
+AVL_Tree_t *AVL_create(void (*freeData)(void *data));
+void AVL_free(AVL_Tree_t *tree);
+void AVL_insert(AVL_Tree_t *tree, void *data);
+void *AVL_remove(AVL_Tree_t *tree, void *data);
+void *AVL_find(void *data);
