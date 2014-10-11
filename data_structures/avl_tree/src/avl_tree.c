0caa4dfdc9357eaf1a6398df2e8ebf3b676315e2
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-11 14:10:00 -0400

    Define and document AVLTree_insert().
    data_structures/avl_tree/src/avl_tree.(c, h)
    	-Define and document `AVLTree_insert()`; does not yet support
    	auto-balancing.

--- data_structures/avl_tree/src/avl_tree.c
+++ data_structures/avl_tree/src/avl_tree.c
@@ -11,3 +11,22 @@ AVLTree_Tree_t *AVLTree_create(
 	tree->compareData = compareData;
 	return tree;
 }
+
+void AVLTree_insert(AVLTree_Tree_t *tree, BinaryTree_Node_t *node, void *data){
+	if(tree->compareData(node->data, data) < 0){
+		if(node->right){
+			AVLTree_insert(tree, node->right, data);
+		}
+		else {
+			BinaryTree_insertRight(tree, node, data);
+		}
+	}
+	else {
+		if(node->left){
+			AVLTree_insert(tree, node->left, data);
+		}
+		else {
+			BinaryTree_insertLeft(tree, node, data);
+		}
+	}
+}
