175bc7e62c79a09c6c36591da6a0638519388e32
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-19 22:38:01 -0400

    Debug insert(), define __setitem__().
    data_structures/heap/heap/data_structure.py
    	-`parent_ind` wasn't getting reset inside the `insert()` loop;
    	fix.
    	-Add varargs to `insert()`, to ease bulk inserts.
    	-Rename `__length__()` to `__len__()`.

--- data_structures/heap/heap/data_structure.py
+++ data_structures/heap/heap/data_structure.py
@@ -3,25 +3,30 @@ class Heap(object):
 	def __init__(self):
 		self._items = []
 
-	def insert(self, item):
-		self._items.append(item)
-
+	def insert(self, *items):
 		def get_parent_ind(ind):
 			return int((ind - 1) / 2)
 
-		child_ind = len(self) - 1
-		parent_ind = get_parent_ind(child_ind)
-		while self[parent_ind] < self[child_ind] and child_ind >= 0:
-			temp = self[parent_ind]
-			self[parent_ind] = self[child_ind]
-			self[child_ind] = temp
-			child_ind = parent_ind
+		for item in items:
+			self._items.append(item)
+
+			child_ind = len(self) - 1
+			parent_ind = get_parent_ind(child_ind)
+			while child_ind > 0 and self[parent_ind] < self[child_ind]:
+				temp = self[parent_ind]
+				self[parent_ind] = self[child_ind]
+				self[child_ind] = temp
+				child_ind = parent_ind
+				parent_ind = get_parent_ind(child_ind)
 
 	def remove(self):
 		pass
 
-	def __getitem__(self, ind):
-		return self._items[ind]
+	def __getitem__(self, key):
+		return self._items[key]
 
-	def __length__(self):
+	def __setitem__(self, key, value):
+		self._items[key] = value
+
+	def __len__(self):
 		return len(self._items)
