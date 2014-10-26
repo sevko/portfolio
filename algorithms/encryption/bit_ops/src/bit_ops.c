64de49b3b3538b823ef06f4168882f6dd7b63c1b
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-26 19:31:13 -0400

    Define BitOps_getBit()/BitOps_printBits().
    algorithms/encryption/bit_ops/src/bit_ops.(c, h)
    	-Define `BitOps_getBit()`; use it to define
    	`BitOps_printBits()`.
    	-Add documentation comments to each.

--- algorithms/encryption/bit_ops/src/bit_ops.c
+++ algorithms/encryption/bit_ops/src/bit_ops.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
 
 #include "bit_ops.h"
 
-extern inline void BitOps_setBit(Byte_t *bytes, unsigned int bitPos);
-// extern inline unsigned int BitOps_getBit(
-	// const Byte_t *bytes, unsigned int bitPos
-// );
-// extern inline void BitOps_rotLeft(Byte_t *bytes, unsigned int numBits);
+// extern inline void BitOps_setBit(Byte_t *bytes, int bitPos);
+extern inline int BitOps_getBit(const Byte_t *bytes, int bitPos);
+// extern inline void BitOps_rotLeft(Byte_t *bytes, int numBits);
+
+void BitOps_printBits(Byte_t *bytes, int numBytes){
+	for(int bit = 0; bit < numBytes * CHAR_BIT; bit++){
+		printf("%d", BitOps_getBit(bytes, bit));
+	}
+	puts("");
+}
--- algorithms/encryption/bit_ops/src/bit_ops.h
+++ algorithms/encryption/bit_ops/src/bit_ops.h
@@ -1,13 +1,29 @@
-/**
+ /**
  * @brief An implementation of common bit operations.
  */
 
 #pragma once
 
+#include <limits.h>
+
 typedef unsigned char Byte_t;
 
-inline void BitOps_setBit(Byte_t *bytes, unsigned int bitPos){
-	(void)bytes;
-	(void)bitPos;
-	puts("Hello, world.");
+/**
+ * @brief Retrieve the value of a bit in an array of bytes.
+ * @param bytes One or more bytes.
+ * @param bitPos The bit position (counting from left-to-right, starting at 0)
+ *      of the bit whose value will be retrieved. Must be within the inclusive
+ *      range `[0, CHAR_BIT * len(bytes) - 1]`.
+ * @return The value of the bit in `bytes` at index `bitPos`; either 0 or 1.
+ */
+inline int BitOps_getBit(const Byte_t *bytes, int bitPos){
+	int shiftDist = CHAR_BIT - 1 - bitPos % CHAR_BIT;
+	return (bytes[bitPos / CHAR_BIT] >> shiftDist) & 1;
 }
+
+/**
+ * @brief Print an array of bytes in binary format (ie, the value of each bit).
+ * @param bytes An array of one or more bytes.
+ * @param numBytes The number of bytes in `bytes`.
+ */
+void BitOps_printBits(Byte_t *bytes, int numBytes);
