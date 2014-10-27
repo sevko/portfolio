08654134c1c46c8c9a4832ce756018813f66bb5d
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-27 09:34:46 -0400

    Define DES-specific 28-bit _rotLeft().
    algorithms/encryption/des/src/des.c
    	-Define `_rotLeft()`, because DES requires that its left
    	rotation operates on the first 28-bits of a byte array, so
    	`BitOps_rotLeft()` wouldn't work (as it operates on entire bytes
    	at a time, and would thus have to select between 24 and 32).

--- algorithms/encryption/des/src/des.c
+++ algorithms/encryption/des/src/des.c
@@ -3,6 +3,28 @@
 
 #include "des.h"
 
+/**
+ * @brief Left-rotate the first 28 bits of 4 8-bit bytes. The last 4 bits are
+ *      passed over when transferring bits shifted off the left end to the
+ *      right end.
+ * @param bytes An array of four bytes (assumed to have eight bits each).
+ * @param rotDist The number of bits to rotate the array by.
+ */
+static void _rotLeft(Byte_t *bytes, int rotDist);
+
 Byte_t *DES_encipher(const Byte_t *plaintext, Byte_t *key){
-	return (Byte_t *)plaintext;
+	return key;
+}
+
+static void _rotLeft(Byte_t *bytes, int rotDist){
+	Byte_t frontShiftedBits = bytes[0] >> (8 - rotDist);
+	bytes[0] <<= rotDist;
+
+	int byte;
+	for(byte = 1; byte < 4; byte++){
+		Byte_t shiftedBits = bytes[byte] >> (8 - rotDist);
+		bytes[byte - 1] |= shiftedBits;
+		bytes[byte] <<= rotDist;
+	}
+	bytes[byte - 1] |= frontShiftedBits << 4;
 }
