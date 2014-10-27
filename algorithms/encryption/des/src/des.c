6ca34e232dc25336d0eab7f0609f11f0d1f6b7a8
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-27 09:49:22 -0400

    Implement permuted block
    algorithms/encryption/des/src/des.c
    	-Implement generation of the first permuted key, and its
    	seventeen permuted blocks.

--- algorithms/encryption/des/src/des.c
+++ algorithms/encryption/des/src/des.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,7 +14,37 @@
 static void _rotLeft(Byte_t *bytes, int rotDist);
 
 Byte_t *DES_encipher(const Byte_t *plaintext, Byte_t *key){
-	return key;
+	static const int permutedChoice1[] = {
+		57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
+		10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+		63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22,
+		14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4
+	};
+
+	Byte_t permutedBlocks[17][2][4] = {{{0}}};
+	int bit;
+	for(bit = 0; bit < 28; bit++){
+		if(BitOps_getBit(key, permutedChoice1[bit] - 1)){
+			BitOps_setBit(permutedBlocks[0][0], bit);
+		}
+	}
+
+	for(; bit < 56; bit++){
+		if(BitOps_getBit(key, permutedChoice1[bit] - 1)){
+			BitOps_setBit(permutedBlocks[0][1], bit - 28);
+		}
+	}
+
+	static int leftRotSchedule[] = {
+		1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
+	};
+	for(int block = 1; block < 17; block++){
+		memcpy(permutedBlocks[block], permutedBlocks[block - 1], 2 * 4);
+		_rotLeft(permutedBlocks[block][0], leftRotSchedule[block - 1]);
+		_rotLeft(permutedBlocks[block][1], leftRotSchedule[block - 1]);
+	}
+
+	return (Byte_t *)plaintext;
 }
 
 static void _rotLeft(Byte_t *bytes, int rotDist){
