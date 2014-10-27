f14cfc2352bc2c8f370314c2b122ce38d60789b2
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-26 20:46:07 -0400

    Add documentation comments to test module.
    algorithms/encryption/bit_ops/src/test.c
    	-Add documentation comments to macros/functions/file header.
    
    makefile
    	-Compile in `libtap`.

--- algorithms/encryption/bit_ops/src/test.c
+++ algorithms/encryption/bit_ops/src/test.c
@@ -1,17 +1,34 @@
+/**
+ * @brief Unit-tests for the `bit_ops` module.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <tap.h>
 
 #include "bit_ops.h"
 
+/**
+ * @brief Define a unit-test for a function. Saves on boilerplate code.
+ * @param funcName The name of the function the unit-test is written for.
+ * @param ... The contents of the test.
+ */
 #define DEF_UNIT_TEST(funcName, ...) \
 	static void _test_## funcName (void){\
 		note("Test " #funcName ".");\
 		__VA_ARGS__\
 	}
 
+/**
+ * @brief Execute the unit-test for a certain function.
+ * @param funcName The name of the function whose unit-test will be executed.
+ *      Must have been defined with `DEF_UNIT_TEST()` earlier on.
+ */
 #define EXEC_UNIT_TEST(funcName) _test_ ##funcName ()
 
+/**
+ * Test `BitOps_getBit()`.
+ */
 DEF_UNIT_TEST(
 	BitOps_getBit,
 	Byte_t bytes[] = {4};
@@ -24,6 +41,9 @@ DEF_UNIT_TEST(
 	}
 )
 
+/**
+ * Test `BitOps_getBitString()`.
+ */
 DEF_UNIT_TEST(
 	BitOps_getBitString,
 	Byte_t bytes[] = {123, 134, 231};
