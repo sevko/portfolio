046f61988cef4e1daf544f47bfc37d110f2b9074
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-26 20:42:42 -0400

    Add tests/helper macros to the test module.
    algorithms/encryption/bit_ops/src/test.c
    	-Define helper function-like macros `DEF_UNIT_TEST()` and
    	`EXEC_UNIT_TEST()`.
    	-Define unit-tests for
    	`BitOps_getBit()`/`BitOps_getBitString()`.

--- algorithms/encryption/bit_ops/src/test.c
+++ algorithms/encryption/bit_ops/src/test.c
@@ -1,10 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <tap.h>
 
 #include "bit_ops.h"
 
+#define DEF_UNIT_TEST(funcName, ...) \
+	static void _test_## funcName (void){\
+		note("Test " #funcName ".");\
+		__VA_ARGS__\
+	}
+
+#define EXEC_UNIT_TEST(funcName) _test_ ##funcName ()
+
+DEF_UNIT_TEST(
+	BitOps_getBit,
+	Byte_t bytes[] = {4};
+	int expectedBits[] = {0, 0, 0, 0, 0, 1, 0, 0};
+	for(int bit = 0; bit < 8; bit++){
+		ok(
+			BitOps_getBit(bytes, bit) == expectedBits[bit],
+			"Bit %d matches.", bit
+		);
+	}
+)
+
+DEF_UNIT_TEST(
+	BitOps_getBitString,
+	Byte_t bytes[] = {123, 134, 231};
+	char *expected = "011110111000011011100111",
+		*actual = BitOps_getBitString(bytes, 3);
+	is(expected, actual, "Actual bit string matches expected.");
+	free(actual);
+)
+
 int main(){
-	Byte_t bytes[4] = {0};
-	BitOps_setBit(bytes, 0);
+	note("Begin unit tests.");
+	EXEC_UNIT_TEST(BitOps_getBit);
+	EXEC_UNIT_TEST(BitOps_getBitString);
+	done_testing();
 	return EXIT_SUCCESS;
 }
