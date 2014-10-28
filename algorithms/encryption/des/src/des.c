93af576601e5efb86394b28c62ff50fd8a321a71
Severyn Kozak <severyn.kozak@gmail.com>
2014-10-28 08:38:49 -0400

    Implement subkey generation.
    algorithms/encryption/des/src/des.c
    	-Implement subkey generation (via permuted choice 2) in
    	`DES_encipher()`.

--- algorithms/encryption/des/src/des.c
+++ algorithms/encryption/des/src/des.c
@@ -44,6 +44,27 @@ Byte_t *DES_encipher(const Byte_t *plaintext, Byte_t *key){
 		_rotLeft(permutedBlocks[block][1], leftRotSchedule[block - 1]);
 	}
 
+	static const int permutedChoice2[] = {
+		14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10,
+		23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2,
+		41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+		44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32
+	};
+	Byte_t subkeys[16][6] = {{0}};
+	for(int subkey = 0; subkey < 16; subkey++){
+		for(bit = 0; bit < 48; bit++){
+			int bitPos = permutedChoice2[bit] - 1;
+			int bitVal = (bitPos < 28) ?
+				BitOps_getBit(permutedBlocks[subkey + 1][0], bitPos) :
+				BitOps_getBit(permutedBlocks[subkey + 1][1], bitPos - 28);
+			if(bitVal){
+				BitOps_setBit(subkeys[subkey], bit);
+			}
+		}
+
+		puts(BitOps_getBitString(subkeys[subkey], 6));
+	}
+
 	return (Byte_t *)plaintext;
 }
 
