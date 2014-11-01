74bea1a1075d18bf457b9c231e85af4a3a4a2542
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-01 12:46:17 -0400

    Define unit-test for DES_encipher().
    algorithms/encryption/des/src/test.c
    	-Remove old, manual testing statements from `main()`.
    	-Define a unit-test for `DES_encipher()`.

--- algorithms/encryption/des/src/test.c
+++ algorithms/encryption/des/src/test.c
@@ -29,6 +29,28 @@
 #define EXEC_UNIT_TEST(funcName) _test_ ##funcName ()
 
 /**
+ * Test `DES_encipher()`.
+ */
+DEF_UNIT_TEST(
+	DES_encipher,
+	const Byte_t key[] = {0xa0, 0x84, 0xe4, 0xf8, 0x9a, 0xb9, 0xcd, 0x1e},
+		plaintext[] = {0xb1, 0x43, 0x17, 0x68, 0x4f, 0xc5, 0x3b, 0xd0},
+		expected[] = {0x87, 0x8C, 0x14, 0xCF, 0xCC, 0xD1, 0xFF, 0x8C};
+	Byte_t *ciphertext = DES_encipher(plaintext, key);
+
+	bool matches = true;
+	for(int byte = 0; byte < 8; byte++){
+		if(ciphertext[byte] != expected[byte]){
+			matches = false;
+			break;
+		}
+	}
+
+	free(ciphertext);
+	ok(matches, "Ciphertext matches expected.");
+)
+
+/**
  * Test `_generateSubkeys()`.
  */
 DEF_UNIT_TEST(
@@ -100,11 +122,12 @@ DEF_UNIT_TEST(
 
 int main(){
 	note("Begin unit tests.");
-	// EXEC_UNIT_TEST(_generateSubkeys);
-	// EXEC_UNIT_TEST(_rotLeft);
-	const Byte_t key[] = {0x13, 0x34, 0x57, 0x79, 0x9b, 0xbc, 0xdf, 0xf1};
-	const Byte_t plaintext[] = {0x1, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
-	DES_encipher(plaintext, key);
+	EXEC_UNIT_TEST(_generateSubkeys);
+	EXEC_UNIT_TEST(_rotLeft);
+	EXEC_UNIT_TEST(DES_encipher);
+
+	// const Byte_t key[] = {0x13, 0x34, 0x57, 0x79, 0x9b, 0xbc, 0xdf, 0xf1};
+	// const Byte_t plaintext[] = {0x1, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
 	done_testing();
 	return EXIT_SUCCESS;
 }
