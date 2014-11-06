a6152a87b929dd7932c4dc155f3a9a5080f59095
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-05 22:46:33 -0500

    Add prototype notes compilation script.
    books/compile_notes/compile.sh
    	-Add an unfinished script to compile all markdown notes to
    	either HTML or LaTeX PDF formats.
    	-Add basic argument handling.

---
+++ books/compile_notes/compile.sh
@@ -0,0 +1,42 @@
+help_message=$(cat << EOF
+usage: bash compile.sh help|(html|latex INPUT_FILE)
+
+	html: Convert ``INPUT_FILE`` to an HTML PDF.
+	latex: Create ``INPUT_FILE`` to a LaTeX PDF.
+	help: Print this help message and exit.
+EOF
+)
+
+misuse_exit(){
+	>&2 echo "$help_message"
+	exit 1
+}
+
+case $1 in
+	html|latex)
+		if [ $# -ne 2 ]; then
+			>&2 echo "Insufficient arguments: missing INPUT_FILE."
+			misuse_exit
+		fi
+
+		case $1 in
+			html)
+				echo "Conv $2 to HTML PDF."
+				;;
+
+			latex)
+				echo "Conv $2 to LaTeX PDF."
+				;;
+		esac
+		;;
+	help)
+		echo "$help_message"
+		;;
+	*)
+		>&2 echo "Unrecognized or missing command."
+		misuse_exit
+		;;
+esac
+
+# pandoc --mathjax 1_boolean_logic.md --template template.html --highlight-style pygments -o 1_boolean_logic.html
+# pandoc --mathjax 1_boolean_logic.md --highlight-style pygments --include-in-header before.tex -o out.pdf
