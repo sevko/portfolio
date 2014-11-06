8704cbe0f57ec6abbb98d401f4916f3cf45ee5ed
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-05 22:50:53 -0500

    Move argument handling to handler_user_args().
    books/compile_notes/compile.sh
    	-Move `misuse_exit` and verbose argument-handling `case`
    	statement to a new function, `handler_user_args()`.
    	-Call it from top-level scope.

--- books/compile_notes/compile.sh
+++ books/compile_notes/compile.sh
@@ -7,36 +7,41 @@ usage: bash compile.sh help|(html|latex INPUT_FILE)
 EOF
 )
 
-misuse_exit(){
-	>&2 echo "$help_message"
-	exit 1
+
+handler_user_args(){
+	misuse_exit(){
+		>&2 echo "$help_message"
+		exit 1
+	}
+
+	case $1 in
+		html|latex)
+			if [ $# -ne 2 ]; then
+				>&2 echo "Insufficient arguments: missing INPUT_FILE."
+				misuse_exit
+			fi
+
+			case $1 in
+				html)
+					echo "Conv $2 to HTML PDF."
+					;;
+
+				latex)
+					echo "Conv $2 to LaTeX PDF."
+					;;
+			esac
+			;;
+		help)
+			echo "$help_message"
+			;;
+		*)
+			>&2 echo "Unrecognized or missing command."
+			misuse_exit
+			;;
+	esac
 }
 
-case $1 in
-	html|latex)
-		if [ $# -ne 2 ]; then
-			>&2 echo "Insufficient arguments: missing INPUT_FILE."
-			misuse_exit
-		fi
-
-		case $1 in
-			html)
-				echo "Conv $2 to HTML PDF."
-				;;
-
-			latex)
-				echo "Conv $2 to LaTeX PDF."
-				;;
-		esac
-		;;
-	help)
-		echo "$help_message"
-		;;
-	*)
-		>&2 echo "Unrecognized or missing command."
-		misuse_exit
-		;;
-esac
+handler_user_args "$@"
 
 # pandoc --mathjax 1_boolean_logic.md --template template.html --highlight-style pygments -o 1_boolean_logic.html
 # pandoc --mathjax 1_boolean_logic.md --highlight-style pygments --include-in-header before.tex -o out.pdf
