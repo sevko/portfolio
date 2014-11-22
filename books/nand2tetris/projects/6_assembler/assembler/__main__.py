e9326dd2848813f1baf1223af5fa3ec5c72082bc
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-22 18:24:58 -0500

    Def assembler.assemble_code().
    books/nand2tetris/projects/6_assembler/assembler/assemble.py
    	-Define `assemble_code()`, which contains the assembler's core
    	logic and aggregates all of its other components.
    	-Currently contains calls to inexistent modules.

--- books/nand2tetris/projects/6_assembler/assembler/__main__.py
+++ books/nand2tetris/projects/6_assembler/assembler/__main__.py
@@ -4,6 +4,9 @@ from assembler import assemble
 
 def handle_user_args(argv):
 	with open(argv[1]) as code_file:
-		print assemble.assemble_code(code_file.read())
+		code = code_file.read()
+
+	for line in assemble.assemble_code(code):
+		print line
 
 handle_user_args(sys.argv)
--- books/nand2tetris/projects/6_assembler/assembler/assemble.py
+++ books/nand2tetris/projects/6_assembler/assembler/assemble.py
@@ -1,2 +1,31 @@
+from assembler import instruction
+from assembler import parser
+from assembler import symbol_table
+
 def assemble_code(code):
-	return "Assembled: \n%s" % code
+	symbols = symbol_table.SymbolTable()
+	instructions = []
+
+	for line in code.split("\n"):
+		clean_line = parser.clean(line)
+
+		# Skip empty lines.
+		if not clean_line:
+			continue
+
+		label = parser.parse_label(clean_line)
+		if label:
+			symbols.add_label(label, len(instructions) + 1)
+			continue
+
+		instr = instruction.parse_instruction(clean_line)
+		if instr:
+			instructions.append(instr)
+		else:
+			# raise Exception("Parse error: `{0}`".format(clean_line))
+			pass
+
+	for instr in instructions:
+		if type(instr) is instruction.AInstr:
+			instr.resolve(symbols)
+		yield instr.to_binary()
