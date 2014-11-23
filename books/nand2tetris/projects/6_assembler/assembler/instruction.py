882c042c54cd54b5b6d04c42f8deb0f7f68dfb41
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-23 17:42:09 -0500

    Implement CInstr.to_binary().
    books/nand2tetris/projects/6_assembler/assembler/instruction.py
    	-Implement `CInstr.to_binary()`; all binary-compilation
    	functions are now defined.

--- books/nand2tetris/projects/6_assembler/assembler/instruction.py
+++ books/nand2tetris/projects/6_assembler/assembler/instruction.py
@@ -48,10 +48,62 @@ class CInstr(object):
 		self.jump = jump
 
 	def to_binary(self):
+		comp_binary = {
+			"0": "0101010",
+			"1": "0111111",
+			"-1": "0111010",
+			"D": "0001100",
+			"A": "0110000",
+			"!D": "0001101",
+			"!A": "0110001",
+			"-D": "0001111",
+			"-A": "0110011",
+			"D+1": "0011111",
+			"A+1": "0110111",
+			"D-1": "0001110",
+			"A-1": "0110010",
+			"D+A": "0000010",
+			"D-A": "0010011",
+			"A-D": "0000111",
+			"D&A": "0000000",
+			"D|A": "0010101",
+			"M": "1110000",
+			"!M": "1110001",
+			"-M": "1110011",
+			"M+1": "1110111",
+			"M-1": "1110010",
+			"D+M": "1000010",
+			"D-M": "1010011",
+			"M-D": "1000111",
+			"D&M": "1000000",
+			"D|M": "1010101"
+		}
 
-		return "C ({0}): {1}, {2}, {3}.".format(
-			"JMP" if self.jump is not None else "INS",
-			self.dest, self.comp, self.jump
+		dest_binary = {
+			None: "000",
+			"M":    "001",
+			"D":    "010",
+			"MD":   "011",
+			"A":    "100",
+			"AM":   "101",
+			"AD":   "110",
+			"AMD":  "111"
+		}
+
+		jump_binary = {
+			None: "000",
+			"JGT": "001",
+			"JEQ": "010",
+			"JGE": "011",
+			"JLT": "100",
+			"JNE": "101",
+			"JLE": "110",
+			"JMP": "111"
+		}
+
+		return "111{0}{1}{2}".format(
+			comp_binary[self.comp], dest_binary[self.dest],
+			jump_binary[self.jump]
 		)
 
 	@classmethod
@@ -60,7 +112,7 @@ class CInstr(object):
 		has_jump = string.find(";") > -1
 
 		if has_dest or has_jump:
-			dest, string = string.split("=") if has_dest else None, string
-			comp, jump = string.split(";") if has_jump else string, None
+			dest, string = string.split("=") if has_dest else (None, string)
+			comp, jump = string.split(";") if has_jump else (string, None)
 
 			return cls(comp, dest=dest, jump=jump)
