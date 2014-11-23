31ecbcf8dec64852c2d984bc3ec0b646ce512a82
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-23 17:44:34 -0500

    Add built-in variables to symbol_table.
    books/nand2tetris/projects/6_assembler/assembler/symbol_table.py
    	-Prepopulate `SymbolTable.table` with the variables the
    	Assembler provides by default.
    	-Fix a bug in `add_variable()`, which wouldn't return the value
    	of an existing variable.

--- books/nand2tetris/projects/6_assembler/assembler/symbol_table.py
+++ books/nand2tetris/projects/6_assembler/assembler/symbol_table.py
@@ -4,9 +4,23 @@ class SymbolTable(object):
 
 	def __init__(self):
 		self.num_variables = 0
-		self.table = {}
+		self.table = {
+			"SP": 0x0000,
+			"LCL": 0x0001,
+			"ARG": 0x0002,
+			"THIS": 0x0003,
+			"THAT": 0x0004,
+			"SCREEN": 0x4000,
+			"KBD": 0x6000
+		}
+
+		for var in xrange(16):
+			self.table["R{0}".format(var)] = var
 
 	def add_variable(self, variable):
+		if variable in self.table:
+			return self.table[variable]
+
 		address = self.VARIABLE_OFFSET + self.num_variables
 		self.num_variables += 1
 		self.table[variable] = address
