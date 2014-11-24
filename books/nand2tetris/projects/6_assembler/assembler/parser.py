f5ef979011d79ebe06b11de21cf2f4de31ebb6a2
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-23 20:31:31 -0500

    Documenth the parser module.
    books/nand2tetris/projects/6_assembler/assembler/parser.py
    	-Add a docstring to the module header, `clean()`, and
    	`parse_label()`.

--- books/nand2tetris/projects/6_assembler/assembler/parser.py
+++ books/nand2tetris/projects/6_assembler/assembler/parser.py
@@ -1,3 +1,7 @@
+"""
+Miscellaneous functions for parsing raw assembly.
+"""
+
 import re
 
 SYMBOL_REGEX = "[{0}][{0}0-9]*".format("a-zA-Z_.$:")
@@ -6,12 +10,29 @@ class AssemblerError(Exception):
 	pass
 
 def clean(string):
+	"""
+	Args:
+		string (string): A raw string, as encountered in unprocessed Assembly.
+
+	Returns:
+		`string` with all whitespace and comments stripped.
+	"""
+
 	remove_regex = ["[ \t]", "//.*$"]
 	for regex in remove_regex:
 		string = re.sub(regex, "", string)
 	return string
 
 def parse_label(string):
+	"""
+	Args:
+		string (string): A raw assembly row (sans newlines), as cleaned with
+			`clean()`, which potentially contains a label.
+
+	Return:
+		The contents of the label, if one was found; otherwise, `None`.
+	"""
+
 	match = re.search("^\({0}\)$".format(SYMBOL_REGEX), string)
 	if match:
 		return match.group().strip("()")
