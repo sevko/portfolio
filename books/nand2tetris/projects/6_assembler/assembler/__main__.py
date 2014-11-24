dbd719e538e92c662a277047e3025014273070ff
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-23 19:33:47 -0500

    Document the __main__ module.
    books/nand2tetris/projects/6_assembler/
    	-Add a docstring to `handle_user_args()`, and the module header.

--- books/nand2tetris/projects/6_assembler/assembler/__main__.py
+++ books/nand2tetris/projects/6_assembler/assembler/__main__.py
@@ -1,9 +1,24 @@
+"""
+The main entry point for the Hack Assembler package, should it be executed
+directly; assembles a source file to machine code.
+"""
+
 import sys
 import os
 
 from assembler import assemble
 
 def handle_user_args(argv):
+	"""
+	Handle user command-line arguments.
+
+	Args:
+		argv (list of string): The arguments passed to the module on the
+			command line. Should have only one: the path to a Hack assembly
+			file, which will be compiled into a raw machine code file with an
+			identical basename.
+	"""
+
 	file_path = argv[1]
 	with open(file_path) as code_file:
 		code = code_file.read()
@@ -13,4 +28,4 @@ def handle_user_args(argv):
 	with open(output_path, "w") as output_file:
 		output_file.write(output + "\n")
 
-handle_user_args(sys.argv)
+handle_user_args(sys.argv[1:])
