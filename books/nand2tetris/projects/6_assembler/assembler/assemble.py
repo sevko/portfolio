95eaf7142ffed43c26eb86270bec70147ba14ad6
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-23 19:34:27 -0500

    Document the assembler module.
    books/nand2tetris/projects/6_assembler/assembler/assemble.py
    	-Add docstrings to the module header and `assemble_code()`.

--- books/nand2tetris/projects/6_assembler/assembler/assemble.py
+++ books/nand2tetris/projects/6_assembler/assembler/assemble.py
@@ -1,8 +1,27 @@
+"""
+The core of the assembler library; aggregates all of its other components.
+"""
+
 from assembler import instruction
 from assembler import parser
 from assembler import symbol_table
 
 def assemble_code(code):
+	"""
+	Assemble an assembly string to machine instructions.
+
+	Args:
+		code (string): The code to assemble.
+
+	Raises:
+		Exception: Thrown when an unrecognized assembly instruction is
+			encountered.
+
+	Yields:
+		(strings) All of the stringified, binary machine instructions, as
+			compiled from `code`.
+	"""
+
 	symbols = symbol_table.SymbolTable()
 	instructions = []
 
