f199119250a526032b5f06c32fc5136c56ff2a4c
Severyn Kozak <severyn.kozak@gmail.com>
2014-11-24 23:10:46 -0500

    Flesh out the translator module. Def translate().
    books/nand2tetris/projects/7_virtual_machine_1_stack_arithmetic/
    vmtranslator/translator.py
    	-Define `translate()`, which relies on as-of-yet unimplemented
    	methods in the package's other modules. Simply aggregates them
    	at a high level of abstraction.
    	-Add a docstring to the module header.

--- books/nand2tetris/projects/7_virtual_machine_1_stack_arithmetic/vmtranslator/translator.py
+++ books/nand2tetris/projects/7_virtual_machine_1_stack_arithmetic/vmtranslator/translator.py
@@ -1,2 +1,22 @@
+"""
+The core of the vmtranslator package. Aggregates all sub-modules and performs
+the actual compilation of VM code to assembly.
+"""
+
+import parser
+
 def translate(code):
-	pass
+	"""
+	Parses and translates a Hack Virtual Machine program into equivalent Hack
+	assembly.
+
+	Args:
+		code (string): The raw VM code, which will be filtered and parsed.
+
+	Returns:
+		(string) The entire assembly program compiled from `code`.
+	"""
+
+	clean_code = parser.clean_code(code)
+	instructions = map(parser.parse_line, clean_code.split("\n"))
+	return "\n".join([instr.to_assembly() for instr in instructions])
