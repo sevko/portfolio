e030860891a8a1d784ae4ca1ce13ce6196a1d512
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-06 17:34:46 -0500

    Work on the parse routine.
    10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
    	-Define a partly functional parse routine, `parse_syntax()`.
    	-The Jack grammar isn't fully implemented in `GRAMMAR`, or
    	supported by the parser yet.

--- books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
+++ books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
@@ -32,3 +32,54 @@ def parse(tokens):
 	out = parse_syntax("class", tokens)
 	print(to_string(out))
 	return out
+
+import sys
+def parse_syntax(syntax_name, tokens, optional=False):
+	syntax = GRAMMAR[syntax_name]
+
+	if isinstance(syntax, str):
+		if tokens[0][0] == syntax:
+			return tokens.pop(0)
+		else:
+			# print(syntax, tokens[0])
+			sys.exit(4)
+
+	structure = []
+	for item in syntax:
+		# print(syntax_name, "-", tokens[0])
+		if isinstance(item, list):
+			for acceptable_match in item:
+				if tokens[0] == acceptable_match:
+					structure.append(tokens.pop(0))
+					break
+			else:
+				# print(item, tokens)
+				sys.exit(1)
+
+		elif isinstance(item, tuple):
+			if len(item) == 2:
+				if tokens[0] == item:
+					structure.append(tokens.pop(0))
+				else:
+					if not optional:
+						# print(syntax_name, item, tokens)
+						sys.exit(2)
+			else:
+				if tokens[0][0] == item[0]:
+					structure.append(tokens.pop(0))
+				else:
+					sys.exit(5)
+
+		else:
+			if item.endswith("*"):
+				item = item[:-1]
+				matches = []
+				match = parse_syntax(item, tokens)
+				while match is not None:
+					matches.append(match)
+					match = parse_syntax(item, tokens, optional=True)
+				structure.append((item, matches))
+			else:
+				structure.append(parse_syntax(item, tokens))
+
+	return (syntax_name, structure)
