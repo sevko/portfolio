1fbc22b9ca606bd5154379f0749a78ad8bf23ce8
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-06 12:14:37 -0500

    Refactor test_syntax, add a Comment test case.
    10_compiler_i_syntax_analysis/test/test_syntax.py
    	-Refactor the `test_syntax` module, abstracting much of the code
    	in `test_keyword_token()` (renamed from `test_keyword()`) into a
    	generalized `_tokenize_test_cases()`, which can be reused for
    	other token-type tests.
    	-Define `test_comment_token()`, identical in structure to the
    	current `test_keyword_token()`.
    	-Add docstrings to `TestTokenizer` and all its functions.

--- books/nand2tetris/projects/10_compiler_i_syntax_analysis/test/test_syntax.py
+++ books/nand2tetris/projects/10_compiler_i_syntax_analysis/test/test_syntax.py
@@ -7,26 +7,44 @@ import unittest
 from jackcompiler.syntax import tokenizer
 
 class TestTokenizer(unittest.TestCase):
+	"""Test `jackcompiler.syntax.tokenizer`.
+	"""
 
-	def setUp(self):
-		pass
+	def test_keyword_token(self):
+		"""Test `KEYWORD` token detection.
+		"""
 
-	def test_keyword(self):
-		keywords = ["class", "constructor/aaaa", "null  ", "while"]
-		non_keywords = ["classa", "random_word"]
+		matches = ["class", "constructor/aaaa", "null  ", "while"]
+		non_matches = ["classa", "random_word"]
+		self._tokenize_test_cases(matches, non_matches, "KEYWORD")
 
-		for keyword in keywords:
-			token = tokenizer.get_token(keyword)
+	def test_comment_token(self):
+		"""Test `COMMENT` token detection.
+		"""
+
+		matches = ["// 'tis a comment", "/*a\nmulti\nline\ncomment*/"]
+		non_matches = ["not // a comment"]
+		self._tokenize_test_cases(matches, non_matches, "COMMENT")
+
+	def _tokenize_test_cases(self, matches, non_matches, token_type):
+		"""
+		Test matches/non-matches against a specified token-type.
+
+		Args:
+			matches (list of string): Text that matches the regex for
+				`token_type`.
+			non_matches (list of string): Text that does not match the regex
+				for `token_type`.
+			token_type (string): The name of the token type to match against
+				(see `jackcompiler.syntax.tokenizer.TOKENS`).
+		"""
+
+		for match in matches:
+			token = tokenizer.get_token(match)
 			self.assertIsInstance(token, tuple)
-			self.assertEqual(token[0], "KEYWORD")
+			self.assertEqual(token[0], token_type)
 
-		for keyword in non_keywords:
-			token = tokenizer.get_token(keyword)
+		for non_match in non_matches:
+			token = tokenizer.get_token(non_match)
 			self.assertIsInstance(token, tuple)
-			self.assertNotEqual(token[0], "KEYWORD")
-
-	def _test_foo(self):
-		print("""
-		FOO
-		FOO
-		FOO""")
+			self.assertNotEqual(token[0], token_type)
