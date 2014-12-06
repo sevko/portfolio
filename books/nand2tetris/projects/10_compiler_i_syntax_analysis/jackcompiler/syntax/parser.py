e83e2e77afed3724691ed0b8cf2b94ddb8745377
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-06 17:33:49 -0500

    Add partial Jack grammar to syntax.parser.
    10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
    	-Add a partially completed Jack grammar dictionary to the
    	`parser` module.

--- books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
+++ books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
@@ -1,2 +1,34 @@
+GRAMMAR = {
+	"class": (
+		("KEYWORD", "class"),
+		"class name",
+		("SYMBOL", "{"),
+		"class var dec",
+		# "subroutine dec*",
+		("SYMBOL", "}")
+	),
+	"class name": "IDENTIFIER",
+	"class var dec": (
+		[("KEYWORD", "static"), ("KEYWORD", "field")],
+		"type",
+		"var name",
+		# "var names*",
+		("SYMBOL", ";")
+	),
+	"type": (
+		[
+			("KEYWORD", "int"), ("KEYWORD", "char"), ("KEYWORD", "boolean"),
+			("IDENTIFIER",)
+		],
+	),
+	"var name": "IDENTIFIER",
+	"var names": (
+		("SYMBOL", ","),
+		"var name"
+	)
+}
+
 def parse(tokens):
-	return tokens
+	out = parse_syntax("class", tokens)
+	print(to_string(out))
+	return out
