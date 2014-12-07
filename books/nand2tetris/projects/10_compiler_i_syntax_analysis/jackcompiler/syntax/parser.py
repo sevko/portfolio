5475cd5e70e08ae562f3d5b4eb54d0e492ec212e
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-06 21:14:05 -0500

    Rewrite the parser substantially.
    10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
    	-Effectively rewrite the parse routine (renamed from
    	`parse_syntax()` to `parse_rule()`), in an attempt to make the
    	code cleaner and more readable, and it to make it more
    	extendable (to allow adding infinitely nested rules, for
    	instance).

--- books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
+++ books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/syntax/parser.py
@@ -1,85 +1,153 @@
 GRAMMAR = {
 	"class": (
-		("KEYWORD", "class"),
-		"class name",
-		("SYMBOL", "{"),
-		"class var dec",
-		# "subroutine dec*",
-		("SYMBOL", "}")
+		("token", ("KEYWORD", "class")),
+		("once", "class name"),
+		("token", ("SYMBOL", "{")),
+		("repeat", ("once", "class var dec")),
+		# ("repeat", ("once", "subroutine dec")),
+		("token", ("SYMBOL", "}"))
 	),
-	"class name": "IDENTIFIER",
+	"class name": (("token type", "IDENTIFIER"),),
 	"class var dec": (
-		[("KEYWORD", "static"), ("KEYWORD", "field")],
-		"type",
-		"var name",
-		# "var names*",
-		("SYMBOL", ";")
+		("either", [
+			("token", ("KEYWORD", "static")),
+			("token", ("KEYWORD", "field"))
+		]),
+		("once", "type"),
+		("once", "var name"),
+		# ("repeat", ("once", "var names")),
+		("token", ("SYMBOL", ";"))
 	),
-	"type": (
-		[
-			("KEYWORD", "int"), ("KEYWORD", "char"), ("KEYWORD", "boolean"),
-			("IDENTIFIER",)
-		],
-	),
-	"var name": "IDENTIFIER",
+	"type": (("either", [
+		("token", ("KEYWORD", "int")),
+		("token", ("KEYWORD", "char")),
+		("token", ("KEYWORD", "boolean")),
+		("token type", ("IDENTIFIER",))
+	]),),
+	"var name": (("token type", "IDENTIFIER"),),
 	"var names": (
-		("SYMBOL", ","),
-		"var name"
+		("token", ("SYMBOL", ",")),
+		("once", "var name")
 	)
 }
 
 def parse(tokens):
-	out = parse_syntax("class", tokens)
-	print(to_string(out))
+	out = parse_rule(GRAMMAR["class"], tokens)
+	# print(to_string(out))
 	return out
 
+class ParserException(Exception):
+	pass
+
 import sys
-def parse_syntax(syntax_name, tokens, optional=False):
-	syntax = GRAMMAR[syntax_name]
+def parse_rule(grammar_rule, tokens, optional=False):
+	grammar_object = []
 
-	if isinstance(syntax, str):
-		if tokens[0][0] == syntax:
-			return tokens.pop(0)
-		else:
-			# print(syntax, tokens[0])
-			sys.exit(4)
+	for component in grammar_rule:
+		print(str(tokens[0]).ljust(30), component)
+		if component[0] == "token":
+			if tokens[0] == component[1]:
+				grammar_object.append(tokens.pop(0))
+			elif not optional:
+				except_msg = "Expecting token `{0}` but got `{1}`.".format(
+					component[1], tokens.pop(0)
+				)
+				raise ParserException(except_msg)
+			else:
+				break
 
-	structure = []
-	for item in syntax:
-		# print(syntax_name, "-", tokens[0])
-		if isinstance(item, list):
-			for acceptable_match in item:
-				if tokens[0] == acceptable_match:
-					structure.append(tokens.pop(0))
+		elif component[0] == "token type":
+			if tokens[0][0] == component[1]:
+				grammar_object.append(tokens.pop(0))
+			elif not optional:
+				except_msg = "Expecting token type `{0}` but got `{1}`".format(
+					component[1], tokens.pop(0)
+				)
+				raise ParserException(except_msg)
+			else:
+				break
+
+		elif component[0] == "token type":
+			if tokens[0][0] == component[1]:
+				grammar_object.append(tokens.pop(0))
+			elif not optional:
+				except_msg = "Expecting token type `{0}` but got `{1}`".format(
+					component[1], tokens.pop(0)
+				)
+				raise ParserException(except_msg)
+			else:
+				break
+
+		elif component[0] == "token type":
+			if tokens[0][0] == component[1]:
+				grammar_object.append(tokens.pop(0))
+			elif not optional:
+				except_msg = "Expecting token type `{0}` but got `{1}`".format(
+					component[1], tokens.pop(0)
+				)
+				raise ParserException(except_msg)
+			else:
+				break
+
+		elif component[0] == "once":
+			match = parse_rule(GRAMMAR[component[1]], tokens, optional=optional)
+			if match:
+				grammar_object.extend(match)
+			elif not optional:
+				except_msg = "Expecting `{0}` but got `{1}`.".format(
+					GRAMMAR[component[1]], tokens[0]
+				)
+				raise ParserException(except_msg)
+			else:
+				break
+
+		elif component[0] == "token type":
+			if tokens[0][0] == component[1]:
+				grammar_object.append(tokens.pop(0))
+			elif not optional:
+				except_msg = "Expecting token type `{0}` but got `{1}`".format(
+					component[1], tokens.pop(0)
+				)
+				raise ParserException(except_msg)
+			else:
+				break
+
+		elif component[0] == "repeat":
+			match = parse_rule([component[1]], tokens, optional=True)
+			while match:
+				grammar_object.extend(match)
+				match = parse_rule([component[1]], tokens, optional=True)
+
+		elif component[0] == "either":
+			for rule in component[1]:
+				match = parse_rule([rule], tokens, optional=True)
+				if match:
+					grammar_object.extend(match)
 					break
 			else:
-				# print(item, tokens)
-				sys.exit(1)
-
-		elif isinstance(item, tuple):
-			if len(item) == 2:
-				if tokens[0] == item:
-					structure.append(tokens.pop(0))
+				if not optional:
+					except_msg = "Expecting any of `{0}` but got `{1}`.".format(
+						component[1], tokens[0]
+					)
+					raise ParserException(except_msg)
 				else:
-					if not optional:
-						# print(syntax_name, item, tokens)
-						sys.exit(2)
-			else:
-				if tokens[0][0] == item[0]:
-					structure.append(tokens.pop(0))
-				else:
-					sys.exit(5)
+					break
 
-		else:
-			if item.endswith("*"):
-				item = item[:-1]
-				matches = []
-				match = parse_syntax(item, tokens)
-				while match is not None:
-					matches.append(match)
-					match = parse_syntax(item, tokens, optional=True)
-				structure.append((item, matches))
-			else:
-				structure.append(parse_syntax(item, tokens))
+		elif component[0] == "optional":
+			match = parse_rule([component[1]], tokens, optional=True)
+			if match:
+				grammar_object.extend(match)
 
-	return (syntax_name, structure)
+	return grammar_object
+
+def to_string(tree):
+	if isinstance(tree[1], str):
+		body = "\t" + tree[1]
+	else:
+		body = [to_string(item) for item in tree[1]]
+		body = "\n".join([
+			"\t" + row
+			for item in tree[1]
+			for row in to_string(item).split("\n")
+		])
+	return "<{0}>\n{1}\n</{0}>".format(tree[0], body)
