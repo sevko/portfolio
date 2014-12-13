bc67391fc0c4d0eb0574450ca2ee509d81c8a761
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-13 17:22:02 -0500

    Document compile_file()/compile_path() in __main__.
    10_compiler_i_syntax_analysis/jackcompiler/__main__.py
    	-Document the `compile_file()`/`compile_path()` methods, just
    	newly added in f00481d.

--- books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/__main__.py
+++ books/nand2tetris/projects/10_compiler_i_syntax_analysis/jackcompiler/__main__.py
@@ -7,12 +7,27 @@ import sys
 import jackcompiler
 
 def compile_file(file_path):
+	"""
+	Compile a single Jack file into a file with an identical basename but an
+	`xml` extension in the current directory.
+
+	Args:
+		file_path (string): The path to a Jack file.
+	"""
+
 	output_path = os.path.splitext(os.path.basename(file_path))[0] + ".xml"
 	with open(file_path) as input_file:
 		with open(output_path, "w") as output_file:
 			output_file.write(jackcompiler.compile_(input_file.read()))
 
 def compile_path(path):
+	"""
+	Args:
+		path (string): The path to either a directory whose files are all
+			assumed to be Jack files, or a single Jack file, to be compiled
+			with `compile_file()`.
+	"""
+
 	if os.path.isfile(path):
 		compile_file(path)
 
