9da669bf577af57fc655c2438bbfb882ae94e1f2
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-14 20:37:16 -0500

    Remove debugging code from compile_node module.
    10_11_jack_compiler/jackcompiler/compiler/compile_node.py
    	-Remove some debugging/testing code: the `Tmp` class, and a
    	bunch of statements from `_compile_varDec()` and
    	`_compile_classVarDec()`.

--- books/nand2tetris/projects/10_11_jack_compiler/jackcompiler/compiler/compile_node.py
+++ books/nand2tetris/projects/10_11_jack_compiler/jackcompiler/compiler/compile_node.py
@@ -10,32 +10,12 @@ def compile_(node, vm_writer, sym_table):
 			if isinstance(child, parser.ParseTreeNode):
 				compile_(child, vm_writer, sym_table)
 
-class Tmp(object):
-
-	def __init__(self, body):
-		self.body = body
-
-	def to_xml(self):
-		return str(self.body)
-
 def _compile_varDec(children, vm_writer, sym_tables):
 	sym_tables["local"].add_symbol(
 		children[2].content, children[1].content, children[0].content
 	)
 
-	sym = sym_tables["local"][children[2].content]
-	children.append(Tmp(sym.name))
-	children.append(Tmp(sym.type_))
-	children.append(Tmp(sym.kind))
-	children.append(Tmp(sym.index))
-
 def _compile_classVarDec(children, vm_writer, sym_tables):
 	sym_tables["global"].add_symbol(
 		children[2].content, children[1].content, children[0].content
 	)
-
-	sym = sym_tables["global"][children[2].content]
-	children.append(Tmp(sym.name))
-	children.append(Tmp(sym.type_))
-	children.append(Tmp(sym.kind))
-	children.append(Tmp(sym.index))
