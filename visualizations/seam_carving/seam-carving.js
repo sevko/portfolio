5688a89c17641defa2ddf806f11ee0ca470e1316
Severyn Kozak <severyn.kozak@gmail.com>
2015-01-18 14:52:39 -0500

    Add seam-carving visualization scaffolding.
    visualizations/seam_carving/(index.html, seam-carving.js)
    	-Setup the seam-carving project, adding files with
    	boilerplate/skeleton layout.

---
+++ visualizations/seam_carving/seam-carving.js
@@ -0,0 +1,19 @@
+!function setup(){
+	var img = new Image();
+	img.src = "image.png";
+	img.onload(function(){
+		console.log("Hello, world!");
+	});
+}();
+
+// return RGBA array
+function carveSeam(img){
+}
+
+// return intensity array
+function gradient(img){
+}
+
+// return gradient
+function pixelGradient(color1, color2){
+}
