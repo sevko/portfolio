9625b5ace0dbdf56daf09ccc82f17c51a9fc6d7a
Severyn Kozak <severyn.kozak@gmail.com>
2015-02-10 23:52:04 -0500

    Functional game of life.
    books/nature_of_code/projects/7_cellular_automata/game_of_life/src/game_of_life.c
    	-Bring the game of life implementation to a functional state.
    	-Define the cell death/life logic in `gameOfLife()`.
    	-Now, refactoring/cleanup required.

--- books/nature_of_code/projects/7_cellular_automata/game_of_life/src/game_of_life.c
+++ books/nature_of_code/projects/7_cellular_automata/game_of_life/src/game_of_life.c
@@ -8,10 +8,10 @@
 #include "game_of_life.h"
 
 typedef struct {
-	bool prev, curr;
+	bool curr, next;
 } Cell_t;
 
-const int NUM_TICKS = 30,
+const int NUM_TICKS = 1000,
 	TICK_US = 1e6 / 30;
 
 inline void drawPixel(SDL_Surface *surface, int x, int y){
@@ -37,7 +37,7 @@ int gameOfLife(void){
 	for(int y = 0; y < height; y++){
 		for(int x = 0; x < width; x++){
 			cells[y][x] = (Cell_t){0};
-			if(rand() % 2 == 1){
+			if(rand() % 15 == 1){
 				cells[y][x].curr = true;
 			}
 		}
@@ -45,15 +45,41 @@ int gameOfLife(void){
 
 	int tick = NUM_TICKS;
 	while(tick--){
+		for(int y = 1; y < height - 1; y++){
+			for(int x = 1; x < width - 1; x++){
+				int numAliveNeighbors =
+					cells[y][x + 1].curr +
+					cells[y][x - 1].curr +
+					cells[y + 1][x].curr +
+					cells[y + 1][x + 1].curr +
+					cells[y + 1][x - 1].curr +
+					cells[y - 1][x].curr +
+					cells[y - 1][x + 1].curr +
+					cells[y - 1][x - 1].curr;
+
+				if(cells[y][x].curr){
+					bool shouldDie = numAliveNeighbors >= 4 ||
+						numAliveNeighbors <= 1;
+					cells[y][x].next = shouldDie ? false : true;
+				}
+				else {
+					cells[y][x].next = (numAliveNeighbors == 3) ? true : false;
+				}
+			}
+		}
+
 		for(int y = 0; y < height; y++){
 			for(int x = 0; x < width; x++){
+				cells[y][x].curr = cells[y][x].next;
 				if(cells[y][x].curr){
 					drawPixel(surface, x, y);
 				}
 			}
 		}
+
 		SDL_Flip(surface);
-		usleep(TICK_US);
+		SDL_FillRect(surface, NULL, 0x000000);
+		// usleep(TICK_US);
 	}
 	return 0;
 }
