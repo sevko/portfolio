4c2290f5e74258b436f8b89a7bae30a994927999
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-10 09:29:14 -0400

    Define ArrayQueue_t interface.
    src/array_queue.h
    	-Forward declare the `ArrayQueue_t` type, and add function
    	declarations for all of the functions the queue is expected to
    	implement.
    
    src/array_queue.c
    	-Add empty function definitions for all of the declarations in
    	`array_queue.h`.

--- data_structures/array_queue/src/array_queue.c
+++ data_structures/array_queue/src/array_queue.c
@@ -1,1 +1,27 @@
 #include "array_queue.h"
+
+ArrayQueue_t *create(void){}
+void ArrayQueue_destroy(ArrayQueue_t *queue){
+	(void)queue;
+}
+
+void ArrayQueue_enqueue(ArrayQueue_t *queue, void *item){
+	(void)queue;
+	(void)item;
+}
+
+void *ArrayQueue_dequeue(ArrayQueue_t *queue){
+	(void)queue;
+}
+
+void *ArrayQueue_peek(ArrayQueue_t *queue){
+	(void)queue;
+}
+
+int ArrayQueue_size(ArrayQueue_t *queue){
+	(void)queue;
+}
+
+bool ArrayQueue_empty(ArrayQueue_t *queue){
+	(void)queue;
+}
--- data_structures/array_queue/src/array_queue.h
+++ data_structures/array_queue/src/array_queue.h
@@ -1,5 +1,18 @@
 /**
- * @brief 
+ * A queue implemented using an array (read: a contiguous block of memory on
+ * the heap) rather than a singly-linked list.
  */
 
 #pragma once
+
+#include <stdbool.h>
+
+typedef struct ArrayQueue ArrayQueue_t;
+
+ArrayQueue_t *create(void);
+void ArrayQueue_destroy(ArrayQueue_t *queue);
+void ArrayQueue_enqueue(ArrayQueue_t *queue, void *item);
+void *ArrayQueue_dequeue(ArrayQueue_t *queue);
+void *ArrayQueue_peek(ArrayQueue_t *queue);
+int ArrayQueue_size(ArrayQueue_t *queue);
+bool ArrayQueue_empty(ArrayQueue_t *queue);
