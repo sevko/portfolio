12fdc1fbf0b3947561a1d2c7bd6e54cab0dca70c
Severyn Kozak <severyn.kozak@gmail.com>
2015-04-11 11:48:17 -0400

    Add a TAP unit-test.
    data_structures/array_queue/(makefile, src/main.c)
    	-Link in `libtap`.
    	-Add a unit test that thoroughly tests all `ArrayQueue_t`
    	methods (though not in a particularly structured way -- ie,
    	there aren't discrete unit tests so much as a cover-all test).

--- data_structures/array_queue/src/main.c
+++ data_structures/array_queue/src/main.c
@@ -1,17 +1,48 @@
 #include <stdio.h>
 #include "array_queue.h"
+#include <tap.h>
 
-int main(){
-	const int NUM_NUMBERS = 9;
-	int numbers[NUM_NUMBERS];
+static void _test_ArrayQueue(void){
 	ArrayQueue_t *queue = ArrayQueue_create();
-	for(int ind = 0; ind < NUM_NUMBERS; ind++){
+	int NUM_TEST_VALUES = 100;
+	int numbers[NUM_TEST_VALUES];
+
+	note("Testing peek(), size(), and empty().");
+	for(int ind = 0; ind < NUM_TEST_VALUES; ind++){
 		numbers[ind] = ind;
 		ArrayQueue_enqueue(queue, &numbers[ind]);
+		ok(
+			*(int *)ArrayQueue_peek(queue) == numbers[0],
+			"Head is %d.", numbers[0]
+		);
+		ok(ArrayQueue_size(queue) == ind + 1, "Size is %d.", ind + 1);
+		ok(!ArrayQueue_empty(queue), "Queue is not empty.");
 	}
+
+	note("Testing peekIndex().");
+	for(int ind = 0; ind < NUM_TEST_VALUES; ind++){
+		ok(
+			*(int *)ArrayQueue_peekIndex(queue, ind) == numbers[ind],
+			"peekIndex(%d) returns %d.", ind, numbers[ind]
+		);
+	}
+
+	note("Testing dequeue().");
+	int ind = 0;
 	while(!ArrayQueue_empty(queue)){
-		printf("Dequeued: %d\n", *(int *)ArrayQueue_dequeue(queue));
+		int dequeued = *(int *)ArrayQueue_dequeue(queue);
+		ok(
+			dequeued == numbers[ind++], "Dequeued value %d matches expected.",
+			dequeued
+		);
 	}
+
 	ArrayQueue_destroy(queue);
+}
+
+int main(){
+	note("Begin tests for ArrayQueue");
+	_test_ArrayQueue();
+	done_testing();
 	return 0;
 }
