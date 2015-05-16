c5450e1c688f620a37feaac06c2fd5ade7be6d8e
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-16 19:18:51 -0400

    Add a prototype sieve of eratosthenes implementation.
    algorithms/sieve_of_eratosthenes/makefile
    	-Add a boilerplate makefile to compile everything.
    
    algorithms/sieve_of_eratosthenes/src/(main.c, sieve_of_eratosthenes.(c, h))
    	-Add some incomplete source.
    	-Define `findNthPrimeNumber()` and a helper function,
    	`nthPrimeUpperBound()`.

---
+++ algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/* Return the upper bound for the `n`th prime. */
+static int nthPrimeUpperBound(int n){
+	if(n >= 6){
+		return n * (log(n) + log(log(n)));
+	}
+	else {
+		static const int smallUpperBounds[] = {3, 4, 6, 8, 12};
+		return smallUpperBounds[n - 1];
+	}
+}
+
+int findNthPrimeNumber(int n){
+	const int upperBound = nthPrimeUpperBound(n);
+	char numbers[upperBound];
+	memset(numbers, 0, upperBound);
+
+	int upperFactorBound = ceil(sqrt(upperBound));
+	for(int factor = 2; factor <= upperFactorBound; factor++){
+		int multiple = factor * 2;
+		while(multiple < upperBound){
+			numbers[multiple] = 1;
+			multiple += factor;
+		}
+	}
+
+	int numPrimes = 0;
+	for(int ind = 2; ind < upperBound; ind++){
+		if(numbers[ind] == 0 && ++numPrimes == n){
+			return ind;
+		}
+	}
+	return -1;
+}
