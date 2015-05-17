22ef65463d532d2e23e82c93403dae268f3ca33c
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-16 23:29:39 -0400

    Break up findNthPrime into another function.
    algorithms/sieve_of_eratosthenes_hs/sieve_of_eratosthenes.hs
    	-Define type synonyms `SieveEntry` and `Sieve`, to make the type
    	signatures of `findNthPrime`'s helper functions cleaner.
    	-Move `checkOffFactors` into the module level, to avoid
    	cluttering `findNthPrime` any more.

--- algorithms/sieve_of_eratosthenes_hs/sieve_of_eratosthenes.hs
+++ algorithms/sieve_of_eratosthenes_hs/sieve_of_eratosthenes.hs
@@ -1,9 +1,20 @@
+type SieveEntry = (Bool, Int)
+type Sieve = [SieveEntry]
+
 upperBoundNthPrime :: Int -> Int
 upperBoundNthPrime n
 	| n >= 7022 = ceiling $ n' * (log n' + log (log n') - 0.9385)
 	| n >= 6 = ceiling $ n' * (log n' + log (log n'))
 	| otherwise = [3, 4, 6, 8, 12] !! (n - 1)
 	where n' = fromIntegral n
+
+checkOffFactors :: Sieve -> Int -> Sieve
+checkOffFactors sieve factor =
+	map (\ entry@(isPrime, num) ->
+		if num <= factor
+			then entry
+			else (isPrime && num `mod` factor /= 0, num))
+	sieve
 
 findNthPrime :: Int -> Int
 findNthPrime n = let
@@ -13,8 +24,8 @@ findNthPrime n = let
 	sieve = foldl checkOffFactors numbers [2..factorUpperBound]
 	in findNthPrimeInSieve n sieve
 	where
-		checkOffFactors numbers factor =
-			map (\ entry@(isPrime, num) -> if num <= factor then entry else (isPrime && num `mod` factor /= 0, num)) numbers
+
+		findNthPrimeInSieve :: Int -> Sieve -> Int
 		findNthPrimeInSieve _ [] = error "nth prime not found in number search-space. Implementation error."
 		findNthPrimeInSieve 0 ((True, num):_) = num
 		findNthPrimeInSieve n' ((True, _):sieve) = findNthPrimeInSieve (n' - 1) sieve
