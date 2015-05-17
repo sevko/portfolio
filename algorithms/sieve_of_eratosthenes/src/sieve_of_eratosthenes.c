5365b4c17cda9e46dbf710b28456b48fb24d35d3
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-16 20:09:06 -0400

    Add a stronger upper bound estimate for certain cases.

--- algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.c
+++ algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.c
@@ -4,13 +4,18 @@
 
 /* Return the upper bound for the `n`th prime. */
 static unsigned int nthPrimeUpperBound(int n){
-	/*
-	 * See here for citations for relevant academic material:
-	 * http://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
-	 * Since the inequality only holds for `n >= 6`, we hardcode the upper
-	 * bounds for lower `n`.
-	 */
-	if(n >= 6){
+	if(n >= 7022){
+		/*
+		 * See page 2 of https://www.maa.org/sites/default/files/jaroma03200545640.pdf
+		 */
+		return n * (log(n) + log(log(n)) - 0.9385);
+	}
+	else if(n >= 6){
+		/*
+		 * See http://en.wikipedia.org/wiki/Prime_number_theorem#Approximations_for_the_nth_prime_number
+		 * Since the inequality only holds for `n >= 6`, we proceed to hardcode
+		 * the upper bounds for lower `n`.
+		 */
 		return n * (log(n) + log(log(n)));
 	}
 	else {
