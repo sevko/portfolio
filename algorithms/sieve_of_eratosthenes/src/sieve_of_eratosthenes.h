25aecc206630e6dc4d0b0357972c4675b601ff66
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-16 21:38:24 -0400

    Add an API documentation comment.
    algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.h
    	-Document `findNthPrimeNumber()` from a user's perspective.
    	-Notably, add (back-of-the-envelope) complexity classifications.

--- algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.h
+++ algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.h
@@ -1,3 +1,12 @@
 #pragma once
 
-int findNthPrimeNumber(int n);
+/*
+ * Find and return the `n`th prime number using the Sieve of Eratosthenes. If
+ * the prime couldn't be computed (likely an implementation error), 0 will be
+ * returned.
+ *
+ * Complexity:
+ *   space: n * log(n)
+ *   time: (n * log(n)) ^ (3 / 2)
+ */
+unsigned int findNthPrimeNumber(int n);
