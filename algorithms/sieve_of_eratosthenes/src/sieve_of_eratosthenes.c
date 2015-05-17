4a21ffaedd1cc36b3ecfe383af691d51684b14fe
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-16 21:19:13 -0400

    Add in-code documentation to findNthPrimeNumber().
    algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.c
    	-Document the method used and a number of added optimizations.
    	-Also document some potentially confusing bits of code.

--- algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.c
+++ algorithms/sieve_of_eratosthenes/src/sieve_of_eratosthenes.c
@@ -25,9 +25,29 @@ static unsigned int nthPrimeUpperBound(int n){
 }
 
 unsigned int findNthPrimeNumber(int n){
+	/**
+	 * This prime finder uses a moderately optimized Sieve of Eratosthenes.
+	 * The range of values is stored on the stack in a `numbers` array (note
+	 * that this is dangerous, because we may very well exceed the maximum
+	 * stack size for large enough `n`). Each index contains a binary value:
+	 * whether the number corresponding to that index has been marked off as a
+	 * multiple of a lesser number (1), or not (0). The `char` type is used to
+	 * reduce the array's footprint, but bit masking would be ideal.. as far as
+	 * space is concerned, anyway. Only odd values starting from 3 are stored
+	 * in the array, since we don't need to bother checking even values for
+	 * primality. Consequently, when we check off the multiples of each
+	 * successive prime number, we only visit the odd ones.
+	 */
+
+	// Convenience macros for converting a number to a slot in the `numbers`
+	// array.
 	#define NUM_TO_IND(num) ((num - 3) / 2)
 	#define IND_TO_NUM(num) (num * 2 + 3)
 
+	// A hack necessary to account for the fact that the `numbers` array starts
+	// storing values from 3 (which is necessary because of the assumption that
+	// every value inside of it is odd), meaning that 2, which is the only even
+	// prime, is simply never accounted for.
 	if(n == 1){
 		return 2;
 	}
@@ -37,7 +57,10 @@ unsigned int findNthPrimeNumber(int n){
 	char numbers[numNumbers];
 	memset(numbers, 0, numNumbers);
 
+	// Checking any factors above the square-root of the upper bound would be
+	// redundant with the already visited factors below it.
 	int upperFactorBound = ceil(sqrt(upperBound));
+
 	int factor = 3;
 	int factorInd = 0;
 	while(factor <= upperFactorBound){
@@ -48,6 +71,8 @@ unsigned int findNthPrimeNumber(int n){
 			multiple += multipleDelta;
 		}
 
+		// Find the next prime number in `numbers`, and use that as the next
+		// factor to check off.
 		while(numbers[++factorInd] == 1);
 		factor = IND_TO_NUM(factorInd);
 	}
@@ -58,5 +83,6 @@ unsigned int findNthPrimeNumber(int n){
 			return IND_TO_NUM(ind);
 		}
 	}
-	return -1;
+
+	return 0;
 }
