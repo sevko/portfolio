0c79d4eb7a92392aa4528e69e8b687686365a476
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-30 10:49:45 -0400

    Add some of the modular exponentiation work.
    algorithms/modular_exponentiation/(ModularExponentiation, Test).hs
    	-Add the two necessary source files: `ModularExponentiation.hs`
    	will contain the actual modular exponentiation implementations,
    	and `Test.hs` the unit-testing functions.
    	-Implement three different modular exponentiation functions.
    	-Define a test function that tests all of them.
    
    algorithms/modular_exponentiation/makefile
    	-Add a `makefile` to compile and run everything.

---
+++ algorithms/modular_exponentiation/ModularExponentiation.hs
@@ -0,0 +1,37 @@
+{-
+ - Various implementations of modular exponentiation with different algorithms
+ - and performance characteristics.
+ -}
+
+{-# LANGUAGE BangPatterns #-}
+
+module ModularExponentiation where
+
+import qualified Data.List as List
+
+{-
+ - A recursive implementation of modular exponentiation.
+ -}
+modularPow :: Integral a => a -> a -> a -> a
+modularPow _ 0 _ = 1
+modularPow base exp' mod' =
+	(base * modularPow base (exp' - 1) mod') `mod` mod'
+
+{-
+ - A tail-recursive implementation of modular exponentiation.
+ -}
+modularPow' :: Integral a => a -> a -> a -> a
+modularPow' base exp' mod' =
+	modularPow'Helper 1 0
+	where
+	modularPow'Helper !accumulator expCounter =
+		if expCounter == exp'
+			then accumulator
+			else modularPow'Helper ((accumulator * base) `mod` mod') $ expCounter + 1
+
+{-
+ - A strict, `foldl`-based implementation of modular exponentiation.
+ -}
+modularPow'' :: Integral a => a -> a -> a -> a
+modularPow'' base exp' mod' =
+	List.foldl' (\ acc _ -> (acc * base) `mod` mod') 1 [1..exp']
