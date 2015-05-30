7e35836c1bb199c836ad61e1745713d4f553ae41
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-30 15:31:39 -0400

    Implement modularInverse in ExtendedEuclideanAlgorithm.
    algorithms/extended_euclidean_algorithm/(ExtendedEuclideanAlgorithm, Test).hs
    	-Use the `bezoutCoefficients` function to implement
    	`modularInverse`, which computes the multiplicative modular
    	inverse of a number. Add a test for it in `Test.hs`.

--- algorithms/extended_euclidean_algorithm/ExtendedEuclideanAlgorithm.hs
+++ algorithms/extended_euclidean_algorithm/ExtendedEuclideanAlgorithm.hs
@@ -8,9 +8,9 @@ module ExtendedEuclideanAlgorithm where
  - An implementation of the Extended Euclidean Algorithm, which, given two
  - integers `a` and `b`, computes the coefficients `x` and `y` that satisfy the
  - Bezout identity: `a * x + b * y = gcd(a, b)`. Both positive and negative
- - values are accepted.
+ - values are accepted. Returns (x, y, gcd(a, b)).
  -}
-bezoutCoefficients :: Int -> Int -> (Int, Int)
+bezoutCoefficients :: Int -> Int -> (Int, Int, Int)
 
 {-
  - Given `b = 0`, we know that `gcd(a, b)` is `abs(a)`, and also that, in the
@@ -18,8 +18,17 @@ bezoutCoefficients :: Int -> Int -> (Int, Int)
  - positive and -1 if `a` is negative. `b` can be anything, so we'll just
  - choose 0.
  -}
-bezoutCoefficients a 0 = (if a < 0 then -1 else 1, 0)
+bezoutCoefficients a 0 = (if a < 0 then -1 else 1, 0, abs a)
 bezoutCoefficients a b = let
 	(quotient, remainder) = divMod a b
-	(firstCoef, secondCoef) = bezoutCoefficients b remainder
-	in (secondCoef, firstCoef - quotient * secondCoef)
+	(firstCoef, secondCoef, gcd') = bezoutCoefficients b remainder
+	in (secondCoef, firstCoef - quotient * secondCoef, gcd')
+
+{-
+ - Computes the multiplicative inverse of `num` modulo `modulus`, or the value
+ - `inverse` that satisfies `num * inverse = 1 (mod modulus)`.
+ -}
+modularInverse :: Int -> Int -> Maybe Int
+modularInverse num modulus = let
+	(inverse, _, gcd') = bezoutCoefficients num modulus
+	in if gcd' /= 1 then Nothing else Just inverse
--- algorithms/extended_euclidean_algorithm/Test.hs
+++ algorithms/extended_euclidean_algorithm/Test.hs
@@ -1,14 +1,14 @@
 {-
- - Tests for `ExtendedEuclideanAlgorithm.bezoutCoefficients`.
+ - Tests for the `ExtEuclidean` module.
  -}
 
 import qualified System.Random as Random
 import qualified Text.Format as Format
 import qualified Data.List.Split as Split
-import qualified ExtendedEuclideanAlgorithm
+import qualified ExtendedEuclideanAlgorithm as ExtEuclidean
 
 {-
- - Test the output of `ExtendedEuclideanAlgorithm.bezoutCoefficients` against a
+ - Test the output of `ExtEuclidean.bezoutCoefficients` against a
  - large number of randomized test-cases.
  -}
 testBezoutCoefficients :: IO ()
@@ -24,8 +24,8 @@ testBezoutCoefficients = do
 
 		testResults = map (\ (a, b) ->
 			let
-				(coef1, coef2) =
-					ExtendedEuclideanAlgorithm.bezoutCoefficients a b
+				(coef1, coef2, _) =
+					ExtEuclidean.bezoutCoefficients a b
 				actualGcd = coef1 * a + coef2 * b
 				expectedGcd = gcd a b
 				result = actualGcd == expectedGcd
@@ -50,13 +50,63 @@ testBezoutCoefficients = do
 				[show num1, show num2, show expected, show actual])
 		testResults
 
+	putStrLn "Testing `bezoutCoefficients`"
 	putStrLn $ "Number of tests: " ++ show numberOfRandomTests ++ ". Results:"
 	putStrLn $ "pass: " ++ show numPass
 	putStrLn $ "fail: " ++ show numFail
 
 	if numFail > 0
-		then error "ERROR: One or more failures!"
+		then putStrLn "ERROR: One or more failures!"
 		else putStrLn "All tests passed!"
 
+{-
+ - Test the output of `ExtEuclidean.modularInverse` against a
+ - large number of randomized test-cases.
+ -}
+testModularInverse :: IO ()
+testModularInverse = do
+	randomGen <- Random.getStdGen
+	let
+		numberOfRandomTests = 1000
+		testCases =
+			Split.chunksOf 2 $
+			take (numberOfRandomTests * 2) $
+			Random.randomRs (-100, 100) randomGen
+
+		evalTestCase [num, modulus] = do
+			let
+				inverse = ExtEuclidean.modularInverse num modulus
+				(_, _, gcd') = ExtEuclidean.bezoutCoefficients num modulus
+			if gcd' == 1
+				then
+					let
+						(Just inverseVal) = inverse
+						modProduct = inverseVal * num `mod` modulus
+
+						-- This'll equal 1 UNLESS `modulus` is negative, in
+						-- which case it'll become `modulus - 1`.
+						expectedGcd = 1 `mod` modulus
+					in if modProduct == expectedGcd
+					then return ()
+					else putStrLn $ Format.format
+						"Failed for {0} modulo {1}! Got {2}, but \
+						\`({2} * {0}) mod {1}` equals {3}, not {4}." $
+						map show
+						[num, modulus, inverseVal, modProduct, expectedGcd]
+
+				else if inverse == Nothing
+					then return ()
+					else putStrLn $ Format.format
+						"Failed for {0} modulo {1}! Expecting `Nothing` \
+						\because gcd({0}, {1}) != 1, but got {2}" $
+						[show num, show modulus, show inverse]
+		evalTestCase _ = error "`testCases` has an incorrect number of \
+			\values per test-case."
+
+	putStrLn "Testing `modularInverse`"
+	mapM_ evalTestCase testCases
+
 main :: IO ()
-main = testBezoutCoefficients
+main = do
+	testBezoutCoefficients
+	testModularInverse
