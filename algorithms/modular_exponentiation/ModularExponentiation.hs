12c1d1ca219c06db6b7a9d667d058b5a700f51f9
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-30 11:02:19 -0400

    Indent `where` clause, add a note about stack overflow.

--- algorithms/modular_exponentiation/ModularExponentiation.hs
+++ algorithms/modular_exponentiation/ModularExponentiation.hs
@@ -10,7 +10,8 @@ module ModularExponentiation where
 import qualified Data.List as List
 
 {-
- - A recursive implementation of modular exponentiation.
+ - A recursive implementation of modular exponentiation. It'll happily overflow
+ - the stack for large enough inputs.
  -}
 modularPow :: Integral a => a -> a -> a -> a
 modularPow _ 0 _ = 1
@@ -24,10 +25,11 @@ modularPow' :: Integral a => a -> a -> a -> a
 modularPow' base exp' mod' =
 	modularPow'Helper 1 0
 	where
-	modularPow'Helper !accumulator expCounter =
-		if expCounter == exp'
-			then accumulator
-			else modularPow'Helper ((accumulator * base) `mod` mod') $ expCounter + 1
+		modularPow'Helper !accumulator expCounter =
+			if expCounter == exp'
+				then accumulator
+			else modularPow'Helper ((accumulator * base) `mod` mod') $
+				expCounter + 1
 
 {-
  - A strict, `foldl`-based implementation of modular exponentiation.
