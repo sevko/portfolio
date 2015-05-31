c52c10cf2f0f4e5e39211b071f9550d4c763f043
Severyn Kozak <severyn.kozak@gmail.com>
2015-05-30 23:12:29 -0400

    Add a functional RSA implementation
    algorithms/encryption/rsa/(makefile, rsa).hs
    	-Add an implementation of RSA that exposes `create`, `decrypt`,
    	and `encrypt` functions. It glues together some of the work done
    	in other mini-projects over a number of recent commits, like
    	implementations of modular exponentiation and the extended
    	Euclidean algorithm.
    	-Add a makefile for compiling and running all the things.
    	-Note that this file should be split into two, `Rsa.hs` and
    	`Test.hs`, to separate RSA logic from testing code (which,
    	currently, is just whatever's in `main`).

---
+++ algorithms/encryption/rsa/rsa.hs
@@ -0,0 +1,62 @@
+import qualified ModularExponentiation as ModularExp
+import qualified ExtendedEuclideanAlgorithm as ExtEuclidean
+import qualified SieveOfEratosthenes
+import qualified Data.List as List
+
+import qualified Text.Format as Format
+
+type PublicKey = Integer
+type PrivateKey = Integer
+type Modulus = Integer
+
+decrypt :: PrivateKey -> Modulus -> Integer -> Integer
+decrypt modulus privateKey ciphertext =
+	ModularExp.modularPow''' ciphertext privateKey modulus
+
+encrypt :: PublicKey -> Modulus -> Integer -> Integer
+encrypt modulus publicKey plaintext =
+	ModularExp.modularPow''' plaintext publicKey modulus
+
+create :: Integer -> Integer -> (Modulus, PublicKey, PrivateKey)
+create p q = let
+	n = p * q
+	totient = (p - 1) * (q - 1)
+	eCandidatePrimes = List.takeWhile (< totient)
+		SieveOfEratosthenes.primeNumbers'
+	eCandidate = List.find (\ prime -> totient `mod` prime /= 0)
+		eCandidatePrimes
+	e = case eCandidate of
+		Just prime -> prime
+		Nothing -> error $
+			"No prime `p` was found that satisfies the following\
+				\constraints:\n\
+				\  - 1 < p < totient\n\
+				\  - gcd(p, totient) = 1 (ie, `p` and `totient` are coprime)\n\
+				\for totient " ++ show totient ++ "."
+
+	d = case ExtEuclidean.modularInverse e totient of
+		Just inverse -> inverse
+		Nothing -> error $ Format.format
+			"e ({0}) is not coprime with the totient ({1})! This is an \
+				\implementation error."
+			[show e, show totient]
+	d' = if d < 0 then d + totient else d
+	in (n, e, d')
+
+main :: IO ()
+main = do
+	let
+		message = 65
+		(modulus, publicKey, privateKey) = create 61 53
+
+	putStrLn $ Format.format
+		"Using modulus {0}, public key {1}, and private key {2}."
+		[show modulus, show publicKey, show privateKey]
+
+	let
+		encrypted = encrypt modulus publicKey message
+		decrypted = decrypt modulus privateKey encrypted
+
+	putStrLn $ Format.format
+		"Message {0} is {1} when encrypted and {2} when decrypted."
+		[show message, show encrypted, show decrypted]
