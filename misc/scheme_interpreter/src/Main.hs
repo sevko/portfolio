48979cce9bdc2fe74e620f0396db0caf8e907a71
Severyn Kozak <severyn.kozak@gmail.com>
2015-06-24 12:16:32 -0400

    Move Main.hs into src/

--- misc/scheme_interpreter/src/Main.hs
+++ misc/scheme_interpreter/src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified System.Environment as Environment
 import qualified Text.ParserCombinators.Parsec as Parsec
+import qualified Data.List as List
 import Text.ParserCombinators.Parsec ((<|>))
 
 data LispVal =
@@ -26,13 +27,13 @@ readExpr input = case Parsec.parse parseExpr "lisp" input of
 		_ -> "Found: " ++ show val
 
 parseExpr :: Parsec.Parser LispVal
-parseExpr = Parsec.choice [parseAtom, parseString, parseNumber]
+parseExpr = Parsec.choice [parseString, parseNumber, parseAtom]
 
 parseString :: Parsec.Parser LispVal
 parseString = do
-	Parsec.char '"'
+	_ <- Parsec.char '"'
 	strContents <- Parsec.many $ parseCharSequence
-	Parsec.char '"'
+	_ <- Parsec.char '"'
 	return $ String strContents
 	where
 		escapableChars = [
@@ -57,7 +58,44 @@ parseAtom = do
 		_ -> Atom atom
 
 parseNumber :: Parsec.Parser LispVal
-parseNumber = Parsec.many1 Parsec.digit >>= (return . Number . read)
+parseNumber = Parsec.choice [
+	parseNumberInBase "#o" Parsec.octDigit octToInt,
+	parseNumberInBase "#x" Parsec.hexDigit hexToInt,
+	parseNumberInBase "#b" (Parsec.oneOf "01") binToInt,
+	(Parsec.optional (Parsec.string "#d") >> (Parsec.many1 Parsec.digit) >>=
+		(return . Number . read))]
+	where parseNumberInBase strPrefix digitParser toIntDecoder = do
+		numStr <- (Parsec.try $ (Parsec.string strPrefix >> (Parsec.many1 digitParser)))
+		return $ Number $ case toIntDecoder numStr of
+			Just num -> num
+			Nothing -> error $
+				"Implementation error: the parser passed a string (" ++
+				numStr ++
+				") containing an invalid digit to an integer decoder."
+
+strToInt :: Int -> (Char -> Maybe Int) -> String -> Maybe Integer
+strToInt base digitToInt =
+	fmap digitsToInt . sequence . map digitToInt
+	where
+		digitsToInt :: [Int] -> Integer
+		digitsToInt digits = sum $ zipWith
+			(\ digit place -> (fromIntegral digit) *
+				(fromIntegral base) ^ place)
+			(reverse digits) [0..]
+
+binToInt :: String -> Maybe Integer
+binToInt = strToInt 2 binDigitToInt
+	where
+		binDigitToInt '0' = Just 0
+		binDigitToInt '1' = Just 1
+		binDigitToInt _ = Nothing
+
+hexToInt :: String -> Maybe Integer
+hexToInt = strToInt 16
+	(\ digit -> List.elemIndex digit $ ['0'..'9'] ++ ['a'..'f'])
+
+octToInt :: String -> Maybe Integer
+octToInt = strToInt 8 (\ digit -> List.elemIndex digit ['0'..'7'])
 
 main :: IO ()
 main = do
@@ -65,3 +103,6 @@ main = do
 	case args of
 		[expr] -> putStrLn $ readExpr expr
 		_ -> error "Usage: ./parser SCHEME_EXPR"
+	print $ binToInt "100"
+	print $ hexToInt "7b"
+	print $ octToInt "131"
