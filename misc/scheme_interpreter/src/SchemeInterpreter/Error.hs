c2a3d0bf2b0a61b867fedc75460d4ffe099d04bf
Severyn Kozak <severyn.kozak@gmail.com>
2015-06-27 20:23:51 -0400

    Add an Errors module.

---
+++ misc/scheme_interpreter/src/SchemeInterpreter/Error.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+{-
+ - Types the interpreter uses to signify parsing/evaluation errors.
+ -}
+
+module SchemeInterpreter.Error where
+
+import qualified Control.Monad.Error as Error
+import qualified SchemeInterpreter.Types as Types
+import qualified Text.Format as Format
+import qualified Text.ParserCombinators.Parsec as Parsec
+
+data LispError =
+	NumArgs Integer [Types.LispVal] |
+	TypeMismatch String Types.LispVal |
+	Parser Parsec.ParseError |
+	BadSpecialForm String Types.LispVal |
+	NotFunction String String |
+	UnboundVar String String |
+	Default String
+
+type ThrowsError = Either LispError
+
+instance Show LispError where
+	show (UnboundVar msg varName) = msg ++ ": " ++ varName
+	show (BadSpecialForm msg form) = msg ++ ": " ++ show form
+	show (NotFunction msg funcName) = msg ++ ": " ++ funcName
+	show (NumArgs numExpected args) = Format.format
+		"Expected {0} args, but got {1} ({2})" [
+			show numExpected,
+			show $ length args,
+			show args
+		]
+	show (TypeMismatch expected got) = Format.format
+		"Expecting type {0}, but got {1}." [expected, show got]
+	show (Parser parseErr) = "Parse error: " ++ show parseErr
+
+instance Error.Error LispError where
+	noMsg = Default "An error has occurred"
+	strMsg = Default
+
+throwError = Error.throwError
