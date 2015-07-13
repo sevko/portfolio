d01d052fa239b5ed55b378c56be102de4f8b8bd6
Severyn Kozak <severyn.kozak@gmail.com>
2015-07-13 19:26:33 -0400

    Begin adding proper unit tests.
    misc/json_parser_c/src/test.c
    	-Remove the existing, ad-hoc testing code (namely everything in
    	`main()` and the `readFile()` function).
    	-Define two functions, `testBadInput()` and `testBadInputs()`,
    	which test whether the parser correctly handles errors.

--- misc/json_parser_c/src/test.c
+++ misc/json_parser_c/src/test.c
@@ -1,41 +1,58 @@
+/**
+ * Unit tests for the JSON parser.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <tap.h>
+#include <string.h>
 
 #include "src/json_parser.h"
 
-char *readFile(char *filepath, int *numBytesRead, bool nullTerminate){
-	FILE *file = fopen(filepath, "r");
-	fseek(file, 0, SEEK_END);
-	int fileLength = ftell(file);
-	rewind(file);
+/**
+ * Test whether parsing `inputStr` throws an error of type `type`.
+ */
+static void testBadInput(const char *inputStr, JsonParserErrorType_t type){
+	note("Testing bad input `%s`\n", inputStr);
+	bool failed;
+	JsonParserError_t error;
+	parse(inputStr, true, strlen(inputStr), &failed, &error);
+	ok(failed, "Boolean set to indicate failures.");
 
-	char *fileBuffer = malloc(nullTerminate ? fileLength + 1 : fileLength);
-	if(fread(fileBuffer, 1, fileLength, file) != (unsigned int)fileLength){
-		fputs("Failed to read file.\n", stderr);
-		exit(1);
+	if(error.type == type){
+		pass("Error type matches expected.");
 	}
-	if(nullTerminate){
-		fileBuffer[fileLength] = '\0';
+	else {
+		fail(
+			"Expected error `%s` but got `%s`.", JsonErrorType_toString(type),
+			JsonErrorType_toString(error.type));
 	}
-	fclose(file);
-	if(numBytesRead != NULL){
-		*numBytesRead = fileLength;
-	}
-	return fileBuffer;
+	JsonParserError_free(&error);
+}
+
+/**
+ * Test whether the parser correctly identifies errors.
+ */
+static void testBadInputs(void){
+	testBadInput("{\"a}", JSON_ERR_EOF);
+	testBadInput("e", JSON_ERR_VALUE);
+	testBadInput("1.]", JSON_ERR_NUMBER);
+	testBadInput("[1.0, \"abc\", 1.]", JSON_ERR_NUMBER);
+	testBadInput("5942.a", JSON_ERR_NUMBER);
+	testBadInput(
+		"{\"a\":123,\"b\":[[[{}, [{{]], 1, {}]}", JSON_ERR_UNEXPECTED_CHAR);
+	testBadInput(
+		"[\"a\", \"b\", \"c\n\"]", JSON_ERR_STR_CONTROL_CHAR);
+	testBadInput(
+		"\"\\u434x\"", JSON_ERR_STR_UNICODE_ESCAPE);
+	testBadInput("ne   ", JSON_ERR_UNEXPECTED_CHAR);
+	testBadInput("nul", JSON_ERR_EOF);
+	testBadInput("{\"a\": [1], 1:4}", JSON_ERR_UNEXPECTED_CHAR);
+	testBadInput("falsd", JSON_ERR_UNEXPECTED_CHAR);
+	testBadInput("\"\\9\"", JSON_ERR_STR_INVALID_ESCAPE);
 }
 
 int main(){
-	bool parseFailed;
-	char *parseErrMsg;
-	char *src = "{\"abc\": \"foo\"}";
-	JsonVal_t parsed = parse(src, &parseFailed, &parseErrMsg);
-	if(parseFailed){
-		puts("Error:");
-		fputs(parseErrMsg, stderr);
-		free(parseErrMsg);
-		return EXIT_FAILURE;
-	}
-	JsonVal_print(&parsed);
-	JsonVal_free(&parsed);
+	testBadInputs();
 	return EXIT_SUCCESS;
 }
